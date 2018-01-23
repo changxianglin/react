094d6e819c5858643b985fb2c4359d891fae7d81
changxianglin <281532612@qq.com>
2018-01-24 00:26:34 +0800

    hot-module-replacement

---
+++ webapp/2-7/client/app.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
+import App from './App.jsx'
+
+// ReactDOM.hydrate(<App />, document.getElementById('root'))
+
+const root = document.getElementById('root')
+const render = Component => {
+    ReactDOM.hydrate(
+        <AppContainer>
+        <Component />
+        </AppContainer>,
+        root
+    )
+}
+
+render(App)
+if(module.hot) {
+    module.hot.accept('./App.jsx', () => {
+        const NextApp = require('./App.jsx').default
+        // ReactDOM.hydrate(<NextApp />, document.getElementById('root'))
+        render(NextApp)
+    })
+}
