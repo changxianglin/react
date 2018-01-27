949e918dcc1ff862be7fb57d39ae80f7f2714b99
changxianglin <281532612@qq.com>
2018-01-28 00:46:34 +0800

    add some function

---
+++ webapp/2-8/build/webpack.config.client.js
@@ -0,0 +1,60 @@
+const path = require('path')
+const webpack = require('webpack')
+const HTMLPlugin = require('html-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+
+const config = {
+    entry: {
+        app:path.join(__dirname, '../client/app.js'),
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.join(__dirname, '../dist'),
+        publicPath: '/public/'
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /.js$/,
+                loader: 'babel-loader',
+                exclude: [
+                    path.join(__dirname, '../node_modules')
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLPlugin({
+            template: path.join(__dirname, '../client/template.html')
+        })
+    ]
+}
+
+if(isDev) {
+    config.entry = {
+        app: [
+            'react-hot-loader/patch',
+            path.join(__dirname, '../client/app.js')
+        ]
+    }
+    config.devServer = {
+        host: '0.0.0.0',
+        port: '8888',
+        contentBase: path.join(__dirname, '../dist'),
+        hot: true,
+        overlay: {
+            errors: true
+        },
+        publicPath: '/public',
+        historyApiFallback: {
+            index: '/public/index.html'
+        }
+    }
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+module.exports = config
---
+++ webapp/2-8/server/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const ReactSSR = require('react-dom/server')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const isDev = process.env.NODE_ENV === 'development'
+
+
+if(!isDev) {
+    const serverEntry = require('../dist/server-entry').default
+    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+    app.use('/public', express.static(path.join(__dirname, '../dist')))
+    app.get("*", function(req, res) {
+        const appString = ReactSSR.renderToString(serverEntry)
+        res.send(template.replace('<app></app>', appString))
+    })
+    }else {
+        const devStatic = require('./util/dev-static')
+        devStatic(app)
+    }
+
+
+app.listen(3333, function() {
+    console.log('server is listening on 3333')
+})
