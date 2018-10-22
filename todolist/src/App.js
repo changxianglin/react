1080f2d6f6966d4d6ab9fb7966b6c1ba9a61ad95
changxianglin <281532612@qq.com>
2018-10-22 23:27:57 +0800

    init

---
+++ todolist/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        yes React is cool
+      </div>
+    );
+  }
+}
+
+export default App;
