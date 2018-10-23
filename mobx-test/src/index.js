8b5db74fed356574e728a728be1d22fcc80e2311
changxianglin <281532612@qq.com>
2018-10-24 00:35:15 +0800

    create project use npm webpack and soso

--- mobx-test/src/index.js
+++ mobx-test/src/index.js
@@ -1,1 +1,50 @@
-document.write('hello world')
+// function Animal() {}
+// function Dog() {}
+//
+// Object.defineProperties(Animal.prototype, {
+//   name: {
+//     value() {
+//       return 'Animal'
+//     }
+//   },
+//   say: {
+//     value() {
+//       return `I'm ${this.name()}`
+//     }
+//   }
+// })
+//
+// Dog.prototype = Object.create(Animal.prototype, {
+//   constructor: {
+//     value: Dog,
+//     enumerable: false
+//   }
+//   name: {
+//     value() {
+//       return 'Dog'
+//     }
+//   }
+// })
+//
+// document.write(new Dog().say())
+
+
+
+class Animal {
+  name() {
+    return 'Animal'
+  }
+  say() {
+    return `I'm ${this.name()}`
+  }
+}
+
+class Dog extends Animal {
+  food = 'bone'
+  
+  name() {
+    return 'Dog'
+  }
+}
+
+console.log(new Dog() instanceof Animal)
