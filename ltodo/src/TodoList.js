b3f1848fb78bc3f27e2b57c8829b17e61ef93e04
changxianglin <281532612@qq.com>
2018-10-26 23:58:23 +0800

    event bind

--- ltodo/src/TodoList.js
+++ ltodo/src/TodoList.js
@@ -1,11 +1,28 @@
 import React, {Component, Fragment} from 'react'
 
 class TodoList extends Component{
+    constructor(props) {
+        super(props)
+        this.state = {
+           inputValue: '',
+           list: []
+        }
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
     render() {
         return(
             <Fragment>
             <div>
-                <input />
+                <input
+                    value = {this.state.inputValue}
+                    onChange={this.handleInputChange.bind(this)}
+                />
                 <button>提交</button>
             </div>
             <div>
