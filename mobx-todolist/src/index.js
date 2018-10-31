7d62ebe2252df97148a924efc9c80b4e7b55a3d9
changxianglin <281532612@qq.com>
2018-11-01 00:27:57 +0800

    todolist finished

--- mobx-todolist/src/index.js
+++ mobx-todolist/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import { observable, action, computed } from 'mobx'
 import { observer, PropTypes as ObservablePropTypes} from 'mobx-react'
@@ -14,6 +14,10 @@ class Todo {
     constructor(title) {
         this.title = title
     }
+
+    @action.bound toggle() {
+        this.finished = !this.finished
+    }
 }
 
 class Store {
@@ -22,6 +26,11 @@ class Store {
     @action.bound createTodo(title) {
         this.todos.unshift(new Todo(title))
     }
+
+    @action.bound removeTodo(todo) {
+        this.todos.remove(todo)
+    }
+
 
     @computed get left() {
         return this.todos.filter(todo => !todo.finished).length
@@ -32,13 +41,28 @@ class Store {
 var store = new Store()
 
 @observer
-class TodoItem extends Comment{
+class TodoItem extends React.Component{
     static propTypes = {
         todo: PropTypes.shape({
             id: PropTypes.number.isRequired,
             title: PropTypes.string.isRequired,
             finished: PropTypes.bool.isRequired
         }).isRequired
+    }
+
+    handleClick = e => {
+        this.props.todo.toggle()
+    }
+
+    render() {
+        const todo = this.props.todo
+        return <Fragment>
+            <input type="checkbox" className="toggle"
+                   defaultChecked={todo.finished}
+                   onClick={this.handleClick}
+            />
+            <span className={['title', todo.finished && 'finished'].join(" ")}>{todo.title}</span>
+        </Fragment>
     }
 }
 
@@ -90,6 +114,8 @@ class TodoList extends React.Component {
             <ul>{todos.map(todo => {
                 return <li key = {todo.id} className="todo-item">
                     <TodoItem todo = {todo} />
+                    <span className="delete"
+                          onClick={e => store.removeTodo(todo)}>X</span>
                 </li>
             })}</ul>
             <footer>{store.left} item(s) unfinished</footer>
