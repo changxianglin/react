3fe91302c5cb0bd7ccae5634abeaa937684efdf3
changxianglin <281532612@qq.com>
2018-11-02 23:59:48 +0800

    new css

---
+++ jianshu/src/style.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle, style } from 'styled-components'
+
+const GlobalStyle = style.body`
+    margin: 0;
+    padding: 0;
+    background: red;
+`
+
+
+export default GlobalStyle
