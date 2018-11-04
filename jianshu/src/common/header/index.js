2b8111788112c6bae2791f6e46e836074f0126f6
changxianglin <281532612@qq.com>
2018-11-04 22:45:14 +0800

    use redux-immutable

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -45,7 +45,7 @@ const Header = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        focused: state.header.get('focused')
+        focused: state.getIn(['header', 'focused'])
     }
 }
 
