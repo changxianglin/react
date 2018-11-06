cebbbd23a3f90b64bede7ae673635e2c0bd0027d
changxianglin <281532612@qq.com>
2018-11-06 23:59:28 +0800

    change

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import { actionCreators } from './store'
@@ -18,7 +18,9 @@ import {
     SearhWrapper
 } from "./style"
 
-const getListArea = ( show ) => {
+class Header extends Component{
+
+    getListArea(show) {
     if(show) {
         return (
             <SearchInfo>
@@ -39,49 +41,50 @@ const getListArea = ( show ) => {
             </SearchInfo>
         )
     } else {
-        return null
+    return null
+    }
+    }
+    render(){
+        return(
+            <HeaderWrapper>
+                <Logo />
+                <Nav>
+                    <NavItem className = "left active">首页</NavItem>
+                    <NavItem className = "left">下载App</NavItem>
+                    <NavItem className = "right">登录</NavItem>
+                    <NavItem className = "right">
+                        <i className="iconfont">&#xe636;</i>
+                    </NavItem>
+                    <SearhWrapper>
+                        <CSSTransition
+                            in = {this.props.focused}
+                            timeout = {200}
+                            classNames = "slide"
+                        >
+                            <NavSearch
+                                className = {this.props.focused ? "focused" : ""}
+                                onFocus = {this.props.handleInputFocus}
+                                onBlur = {this.props.handleInputBlur}
+                            >
+
+                            </NavSearch>
+                        </CSSTransition>
+                        <i className = {this.props.focused ? "focused iconfont" : "iconfont"}>&#xe6cf;</i>
+                        {this.getListArea(this.props.focused)}
+                    </SearhWrapper>
+                </Nav>
+                <Addition>
+                    <Button className = "writting">
+                        <i className="iconfont">&#xe615;</i>
+                        写文章
+                    </Button>
+                    <Button className = "reg">注册</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
     }
 }
 
-const Header = (props) => {
-    return (
-        <HeaderWrapper>
-            <Logo />
-            <Nav>
-                <NavItem className = "left active">首页</NavItem>
-                <NavItem className = "left">下载App</NavItem>
-                <NavItem className = "right">登录</NavItem>
-                <NavItem className = "right">
-                    <i className="iconfont">&#xe636;</i>
-                </NavItem>
-                <SearhWrapper>
-                    <CSSTransition
-                        in = {props.focused}
-                        timeout = {200}
-                        classNames = "slide"
-                    >
-                        <NavSearch
-                            className = {props.focused ? "focused" : ""}
-                            onFocus = {props.handleInputFocus}
-                            onBlur = {props.handleInputBlur}
-                        >
-
-                        </NavSearch>
-                    </CSSTransition>
-                    <i className = {props.focused ? "focused iconfont" : "iconfont"}>&#xe6cf;</i>
-                    {getListArea(props.focused)}
-                </SearhWrapper>
-            </Nav>
-            <Addition>
-                <Button className = "writting">
-                    <i className="iconfont">&#xe615;</i>
-                    写文章
-                </Button>
-                <Button className = "reg">注册</Button>
-            </Addition>
-        </HeaderWrapper>
-    )
-}
 
 const mapStateToProps = (state) => {
     return {
