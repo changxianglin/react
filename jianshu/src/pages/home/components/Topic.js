cf92ab819d08c2c047a8868bdd688f6354415c07
changxianglin <281532612@qq.com>
2018-11-09 01:07:08 +0800

    better use redux

--- jianshu/src/pages/home/components/Topic.js
+++ jianshu/src/pages/home/components/Topic.js
@@ -14,6 +14,7 @@ class Topic extends Component{
                                 <img
                                     className="topic-pic"
                                     src={item.get('imgUrl')}
+                                    alt = "/"
                                 />
                                 {item.get('title')}
                             </TopicItem>
@@ -26,7 +27,7 @@ class Topic extends Component{
 }
 
 const mapState = (state) => ({
-    list: state.get('home').get('topicList')
+    list: state.getIn(["home", "topicList"])
 })
 
 export default connect(mapState, null)(Topic)
--- jianshu/src/pages/home/store/reduce.js
+++ jianshu/src/pages/home/store/reduce.js
@@ -5,10 +5,31 @@ const defaultState = fromJS({
         id: 1,
         title: "社会热点",
         imgUrl: "//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64"
-    },{
+    }, {
         id: 2,
         title: "手绘",
         imgUrl: "//upload.jianshu.io/collections/images/283250/%E6%BC%AB%E7%94%BB%E4%B8%93%E9%A2%98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64",
+    }],
+    articleList: [{
+        id: 1,
+        title: "霍建华退出微博：生而为人，我劝你嘴下积德",
+        desc: "1 昨天，霍建华的华杰工作室在微博和facebook宣布关闭运营账号。 工作室的声明里这样写道：从今以后，只想用最纯粹的方式和大家交流，就是用影...",
+        imgUrl: "//upload-images.jianshu.io/upload_images/102559-867f8d0cfbe86f98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240",
+    }, {
+        id: 2,
+        title: "霍建华退出微博：生而为人，我劝你嘴下积德",
+        desc: "1 昨天，霍建华的华杰工作室在微博和facebook宣布关闭运营账号。 工作室的声明里这样写道：从今以后，只想用最纯粹的方式和大家交流，就是用影...",
+        imgUrl: "//upload-images.jianshu.io/upload_images/102559-867f8d0cfbe86f98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240",
+    }, {
+        id: 3,
+        title: "霍建华退出微博：生而为人，我劝你嘴下积德",
+        desc: "1 昨天，霍建华的华杰工作室在微博和facebook宣布关闭运营账号。 工作室的声明里这样写道：从今以后，只想用最纯粹的方式和大家交流，就是用影...",
+        imgUrl: "//upload-images.jianshu.io/upload_images/102559-867f8d0cfbe86f98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240",
+    }, {
+        id: 4,
+        title: "霍建华退出微博：生而为人，我劝你嘴下积德",
+        desc: "1 昨天，霍建华的华杰工作室在微博和facebook宣布关闭运营账号。 工作室的声明里这样写道：从今以后，只想用最纯粹的方式和大家交流，就是用影...",
+        imgUrl: "//upload-images.jianshu.io/upload_images/102559-867f8d0cfbe86f98.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240",
     }]
 })
 
