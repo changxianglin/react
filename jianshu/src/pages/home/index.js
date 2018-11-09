7e4f10ed72e314894cc6abcd3e52ddf395c5ff21
changxianglin <281532612@qq.com>
2018-11-10 00:17:34 +0800

    ajax request data redux change state

--- jianshu/src/pages/home/index.js
+++ jianshu/src/pages/home/index.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Topic from './components/Topic'
 import List from './components/List'
 import Recommend from './components/Recommend'
 import Writer from './components/Writer'
+import axios from 'axios'
+
 import {
     HomeWrapper,
     HomeLeft,
@@ -25,7 +28,24 @@ class Home extends Component{
             </HomeWrapper>
         )
     }
-
+    componentDidMount(){
+        axios.get("/api/home.json").then((res) => {
+            const result = res.data.data
+            const action = {
+                type: "change_home_data",
+                topicList: result.topicList,
+                articleList: result.articleList,
+                recommendList: result.recommendList,
+            }
+            this.props.changeHomeData(action)
+        })
+    }
 }
 
-export default Home
+const mapDispatch = (dispatch) => ({
+    changeHomeData(action) {
+        dispatch(action)
+    }
+})
+
+export default connect(null, mapDispatch)(Home)
--- jianshu/src/pages/home/store/reduce.js
+++ jianshu/src/pages/home/store/reduce.js
@@ -8,6 +8,12 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
     switch (action.type) {
+        case "change_home_data":
+            return state.merge({
+                topicList: fromJS(action.topicList),
+                articleList: fromJS(action.articleList),
+                recommendList: fromJS(action.recommendList),
+            })
         default:
             return state
 
