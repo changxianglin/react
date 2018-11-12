33496f188218e460b8f111924bb49c101f5b5e93
changxianglin <281532612@qq.com>
2018-11-13 00:00:30 +0800

    detail style

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import { DetailWrapper, Header, Contnent } from './style'
 
 class Detail extends Component{
     render() {
         return (
-            <div>Detail~</div>
+            <DetailWrapper>
+                <Header>
+                    我与蒋方舟？
+                </Header>
+                <Contnent>
+                    <img src="//upload-images.jianshu.io/upload_images/3960383-cdbf5ecea69c8491.jpg?imageMogr2/auto-orient/strip%7CimageView2/2/w/750/format/webp" alt=""/>
+                    <p><b>第一次有人说，我和蒋方舟长得像，其实没太在意，</b>因为这个人，我不认识，我在这里承认自己太low啦……</p>
+                    <p>第一次有人说，我和蒋方舟长得像，其实没太在意，因为这个人，我不认识，我在这里承认自己太low啦……</p>
+                    <p>第一次有人说，我和蒋方舟长得像，其实没太在意，因为这个人，我不认识，我在这里承认自己太low啦……</p>
+                    <p>第一次有人说，我和蒋方舟长得像，其实没太在意，因为这个人，我不认识，我在这里承认自己太low啦……</p>
+                </Contnent>
+            </DetailWrapper>
         )
     }
 
