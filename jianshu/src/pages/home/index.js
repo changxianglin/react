07c13cf5295eef050a6f1349c30209b29727c07c
changxianglin <281532612@qq.com>
2018-11-12 22:06:20 +0800

    backtop

--- jianshu/src/pages/home/index.js
+++ jianshu/src/pages/home/index.js
@@ -5,6 +5,7 @@ import List from './components/List'
 import Recommend from './components/Recommend'
 import Writer from './components/Writer'
 import { actionCreators } from './store'
+import { BackTop } from './style'
 
 import {
     HomeWrapper,
@@ -13,6 +14,9 @@ import {
 } from './style'
 
 class Home extends Component{
+    handleScrollTop() {
+        window.scrollTo(0, 0)
+    }
     render() {
         return (
             <HomeWrapper>
@@ -25,19 +29,39 @@ class Home extends Component{
                     <Recommend/>
                     <Writer/>
                 </HomeRight>
+                { this.props.showScroll ? <BackTop onClick = {this.handleScrollTop}>回到顶部</BackTop> : null }
             </HomeWrapper>
         )
     }
     componentDidMount(){
         this.props.changeHomeData()
+        this.bindEvents()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.props.changeScrollTopShow)
+    }
+
+    bindEvents() {
+        window.addEventListener("scroll", this.props.changeScrollTopShow)
     }
 }
 
+const mapState = (state) => ({
+    showScroll: state.getIn(['home', 'showScroll'])
+})
+
 const mapDispatch = (dispatch) => ({
     changeHomeData() {
-        const action = actionCreators.getHomeInfo()
-        dispatch(action)
+        dispatch(actionCreators.getHomeInfo())
+    },
+    changeScrollTopShow() {
+        if(document.documentElement.scrollTop > 100) {
+            dispatch(actionCreators.toggleTopShow(true))
+        } else {
+            dispatch(actionCreators.toggleTopShow(false))
+        }
     }
 })
 
-export default connect(null, mapDispatch)(Home)
+export default connect(mapState, mapDispatch)(Home)
--- jianshu/src/pages/home/store/actionCreators.js
+++ jianshu/src/pages/home/store/actionCreators.js
@@ -34,3 +34,8 @@ export const getMoreList = (page) => {
         })
     }
 }
+
+export const toggleTopShow = (show) => ({
+    type: constants.TOGGLE_SCROLL_TOP,
+    show
+})
