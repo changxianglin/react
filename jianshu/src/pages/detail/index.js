0a3a25e9f5ca7b8078dfa59b4f4d1d87f9c83fd1
changxianglin <281532612@qq.com>
2018-11-14 00:00:08 +0800

    axios data

--- jianshu/src/pages/detail/index.js
+++ jianshu/src/pages/detail/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { DetailWrapper, Header, Contnent } from './style'
+import { actionCreators } from './store'
 
 class Detail extends Component{
     render() {
@@ -13,7 +14,9 @@ class Detail extends Component{
             </DetailWrapper>
         )
     }
-
+    componentDidMount() {
+        this.props.getDetail()
+    }
 }
 
 const mapState = (state) => ({
@@ -21,4 +24,10 @@ const mapState = (state) => ({
     content: state.getIn(['detail', 'content'])
 })
 
-export default connect(mapState, null)(Detail)
+const mapDispatch = (dispatch) => ({
+    getDetail() {
+        dispatch(actionCreators.getDetail())
+    }
+})
+
+export default connect(mapState, mapDispatch)(Detail)
