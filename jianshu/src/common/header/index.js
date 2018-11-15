f4333efa1b8633d0dd784219cfdae63b9cb4eb1d
changxianglin <281532612@qq.com>
2018-11-16 00:08:32 +0800

    link to login

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -57,7 +57,7 @@ class Header extends Component{
     }
     }
     render(){
-        const { focused, handleInputFocus, handleInputBlur, list } = this.props
+        const { focused, handleInputFocus, handleInputBlur, list, login } = this.props
         return(
             <HeaderWrapper>
                 <Link to = "/">
@@ -66,7 +66,10 @@ class Header extends Component{
                 <Nav>
                     <NavItem className = "left active">首页</NavItem>
                     <NavItem className = "left">下载App</NavItem>
-                    <NavItem className = "right">登录</NavItem>
+                    {
+                        login ? <NavItem className = "right">退出</NavItem>
+                            : <Link to = '/login'><NavItem className = "right">登录</NavItem></Link>
+                    }
                     <NavItem className = "right">
                         <i className="iconfont">&#xe636;</i>
                     </NavItem>
@@ -108,6 +111,7 @@ const mapStateToProps = (state) => {
         page: state.getIn(['header', 'page']),
         totalPage: state.getIn(['header', 'totalPage']),
         mouseIn: state.getIn(['header', 'mouseIn']),
+        login: state.getIn(['login', 'login']),
     }
 }
 
