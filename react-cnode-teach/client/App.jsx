fe5af4bf660e48a8671700b41018c138111f4462
changxianglin <281532612@qq.com>
2018-11-20 00:17:18 +0800

    hot-module-repalcement

--- react-cnode-teach/client/App.jsx
+++ react-cnode-teach/client/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default class App extends React.Component {
     render() {
         return (
-            <div>This is app</div>
+            <div>This is app123</div>
         )
     }
 }
--- react-cnode-teach/client/app.js
+++ react-cnode-teach/client/app.js
@@ -1,5 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 import App from './App.jsx'
 
-ReactDOM.hydrate(<App />, document.getElementById('root'))
+// ReactDOM.hydrate(<App />, )
+
+const root = document.getElementById('root')
+
+const render = Component => {
+    ReactDOM.hydrate(
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        root
+    )
+}
+
+render(App)
+
+if(module.hot) {
+    module.hot.accept('./App.jsx', () => {
+        const NextApp = require('./App.jsx').default
+        // ReactDOM.hydrate(<NextApp />, document.getElementById('root'))
+        render(NextApp)
+    })
+}
