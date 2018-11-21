10e7a4e28cd0f6e83c265cb4f987aea48e1c3cf1
changxianglin <281532612@qq.com>
2018-11-21 23:59:07 +0800

    ssr config

--- react-cnode-teach/server/server.js
+++ react-cnode-teach/server/server.js
@@ -2,19 +2,25 @@ const express = require('express')
 const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
-const serverEntry = require('../dist/server-entry').default
 
-const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+const isDev = process.env.NODE_ENV === 'development'
+
 
 const app = express()
 
-app.use('/public', express.static(path.join(__dirname, '../dist')))
+if(!isDev) {
+    const serverEntry = require('../dist/server-entry').default
+    const template = fs.readFileSync(path.join(__dirname, '../dist/index.html'), 'utf8')
+    app.use('/public', express.static(path.join(__dirname, '../dist')))
+    app.get('*', function (req, res) {
+        const appString = ReactSSR.renderToString(serverEntry)
 
-app.get('*', function (req, res) {
-    const appString = ReactSSR.renderToString(serverEntry)
-
-    res.send(template.replace('<!-- app -->', appString))
-})
+        res.send(template.replace('<!-- app -->', appString))
+    })
+} else {
+    const devStatic = require('./util/dev-static')
+    devStatic(app)
+}
 
 app.listen(3333, function() {
     console.log('server is  listening on 3333')
---
+++ react-cnode-teach/server/util/dev-static.js
@@ -0,0 +1,51 @@
+const axios = require('axios')
+const path = require('path')
+const webpack = require('webpack')
+const MemoryFs = require('memory-fs')
+const ReactDomServer = require('react-dom/server')
+
+const serverConfig = require('../../build/webpack.config.server')
+
+
+const getTemplate = () => {
+    return new Promise((resolve, reject) => {
+        axios.get('http://localhost:8888/public/index.html').then(res => {
+            resolve(res.data)
+        }).catch(reject)
+    })
+}
+
+const Module = module.constructor
+
+const mfs = new MemoryFs()
+const serverCompiler = webpack(serverConfig)
+serverCompiler.outputFileSystem = mfs
+let serverBundle
+serverCompiler.watch({}, (err, stats) => {
+    if(err) throw err
+    stats = stats.toJson()
+    stats.errors.forEach(err => console.log(err))
+    stats.warnings.forEach(warn => console.log(warn))
+
+
+    const bundlePath = path.join(
+        serverConfig.output.path,
+        serverConfig.output.filename
+    )
+
+    const bundle = mfs.readFileSync(bundlePath)
+    const m = new Module()
+    m._compile(bundle, 'server-entry.js')
+    serverBundle = m.default
+})
+
+
+module.exports = function (app) {
+    
+    app.get('*', function (req, res) {
+        getTemplate().then(template => {
+            const content = ReactDomServer.renderToString(serverBundle, 'utf-8')
+            res.send(template.replace('<!-- app -->', content))
+        })
+    })
+}
