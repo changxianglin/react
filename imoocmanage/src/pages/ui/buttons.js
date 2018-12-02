674ed4be7c1b1eef0f830ea137247752a0af6a7f
changxianglin <281532612@qq.com>
2018-12-02 22:49:52 +0800

    some button

--- imoocmanage/src/pages/ui/buttons.js
+++ imoocmanage/src/pages/ui/buttons.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Card, Button } from 'antd'
+import './ui.less'
 
 export default class Buttons extends React.Component {
     render() {
         return (
             <div>
-                This is Buttons page
+                <Card title = "基础按钮">
+                    <Button type = "primary">Imooc</Button>
+                    <Button >Imooc</Button>
+                    <Button type = "dashed">Imooc</Button>
+                    <Button type = "danger">Imooc</Button>
+                    <Button disabled>Imooc</Button>
+                </Card>
+                <Card title = "图像按钮">
+                    <Button icon = "plus">创建</Button>
+                    <Button icon = "edit">编辑</Button>
+                    <Button icon = "delete">删除</Button>
+                    <Button shape = "circle" icon = "search"></Button>
+                    <Button type = "primary" icon = "search">搜索</Button>
+                    <Button type = "primary" icon = "download">下载</Button>
+                </Card>
+                <Card title = "Loading按钮">
+                    <Button type = "primary" loading = {true}>确定</Button>
+                    <Button type = 'primary' shape = "circle" loading = {true}></Button>
+                    <Button loading = {true}>点击加载</Button>
+                    <Button shape = "circle" loading = {true}></Button>
+                    <Button type = "primary" onClick = {this.handleColse}>关闭</Button>
+                </Card>
             </div>
         )
     }
