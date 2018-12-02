f30ae2142c4b15704b8e9d53cd849949a6bf4049
changxianglin <281532612@qq.com>
2018-12-03 01:28:56 +0800

    from getValue antd

--- imoocmanage/src/pages/form/login.js
+++ imoocmanage/src/pages/form/login.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import { Card, Form, Input, Button} from 'antd'
+import { Card, Form, Input, Button, message, Icon, Checkbox } from 'antd'
 import FormItem from 'antd/lib/form/FormItem';
 
 const FromItem = Form.Item
 
 class FormLogin extends React.Component {
+    handleSubmit = () => {
+        let userInfo = this.props.form.getFieldsValue()
+        this.props.form.validateFields((err, values) => {
+            if(!err) {
+                message.success(`${userInfo.userName} 恭喜你，您通过本次表单学习，当前密码为：${userInfo.userPwd}`)
+            }
+        })
+    }
+
     render() {
         const { getFieldDecorator } = this.props.form 
         return (
@@ -27,25 +36,56 @@ class FormLogin extends React.Component {
                         <FromItem>
                             {
                                 getFieldDecorator("userName", {
-                                    initialValue: 'zhangsan',
-                                    rules: []
+                                    initialValue: '',
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: "用户名不能为空"
+                                        },
+                                        {
+                                           min: 5,
+                                           max: 10,
+                                           message: "长度不在范围内" 
+                                        },
+                                        {
+                                           pattern: new RegExp("^\\w+$", "g"),
+                                           message: "用户名必须为英文字母或数字" 
+                                        }
+                                    ]
                                 })(
-                                    <Input placeholder = "请输入用户名" />
+                                    <Input prefix = {<Icon type = "user" />} placeholder = "请输入用户名" />
                                 )
                             }
                         </FromItem>
                         <FromItem>
                             {
-                                getFieldDecorator("userName", {
-                                    initialValue: '123456',
-                                    rules: []
+                                getFieldDecorator("userPwd", {
+                                    initialValue: '',
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: "密码不能为空"
+                                        }
+                                    ]
                                 })(
-                                    <Input placeholder = "请输入密码" />
+                                    <Input prefix = {<Icon type = "lock" />} type = "password" placeholder = "请输入密码" />
                                 )
                             }
                         </FromItem>
                         <FromItem>
-                            <Button type = "primary">登录</Button>
+                            {
+                                getFieldDecorator("remember", {
+                                    valuePropName: "checked",
+                                    initialValue: true,
+                                    rules: []
+                                })(
+                                   <Checkbox>记住密码</Checkbox>
+                                )
+                            }
+                            <a href = "#" style = {{float: "right"}}>忘记密码</a>
+                        </FromItem>
+                        <FromItem>
+                            <Button type = "primary" onClick = {this.handleSubmit}>登录</Button>
                         </FromItem>
                     </Form>
                 </Card>
