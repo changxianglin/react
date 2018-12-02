6939e84c6b028bd824f22d46f7ba0f077f171f34
changxianglin <281532612@qq.com>
2018-12-02 17:26:42 +0800

    use basic react router

--- imoocmanage/src/index.js
+++ imoocmanage/src/index.js
@@ -4,9 +4,10 @@ import './index.css';
 import App from './App';
 import Life from './pages/demo/Life'
 import Admin from './admin'
+import Home from './pages/route_demo/route1/Home'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Admin />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ imoocmanage/src/pages/route_demo/route1/topic.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Topic extends React.Component {
+    render() {
+        return (
+            <div>
+            This is Topic page
+        </div>
+        )
+    }
+}
