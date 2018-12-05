10ed7fd8a1a186431916f8813cbe39d783711690
changxianglin <281532612@qq.com>
2018-12-06 00:58:14 +0800

    table about checkbox

--- imoocmanage/src/pages/table/basicTable.js
+++ imoocmanage/src/pages/table/basicTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Table, Modal} from 'antd'
+import { Card, Table, Modal, Button, message} from 'antd'
 import axios from './../../axios/index'
 
 export default class BasicTable extends React.Component {
@@ -65,7 +65,9 @@ export default class BasicTable extends React.Component {
                     item.key = index
                 })
                 this.setState({
-                    dataSource2: res.result
+                    dataSource2: res.result,
+                    selectedRowKeys: [],
+                    selectedRows: null 
                 })
             }
         })
@@ -82,6 +84,22 @@ export default class BasicTable extends React.Component {
             selectedItem: record
         })   
     }
+
+    handleDelete = (() => {
+        let rows = this.state.selectedRows
+        let ids = []
+        rows.map((item) => {
+            ids.push(item.id)
+        })
+        Modal.confirm({
+            title: "删除提示",
+            content: `您确定要删除这些数据吗?${ids.join(',')}`,
+            onOk: () => {
+                message.success("删除成功")
+                this.request()
+            }
+        })
+    })
 
     render() {
         const columns = [
@@ -151,6 +169,16 @@ export default class BasicTable extends React.Component {
             selectedRowKeys
         }
 
+        const rowCheckSelection = {
+            type: 'checkbox',
+            selectedRowKeys,
+            onChange: (selectedRowKeys, selectedRows) => {
+                this.setState({
+                    selectedRowKeys,
+                    selectedRows
+                })
+            }
+        }
         return (
             <div>
                 <Card title = "基础表格">
@@ -181,6 +209,18 @@ export default class BasicTable extends React.Component {
                             }
                         }
                         rowSelection = {rowSelection}
+                        dataSource = {this.state.dataSource2}
+                        columns = {columns}
+                        pagination = {false}
+                    />
+                </Card>
+                <Card title = "mock-多选" style = {{margin: "10px 0"}}>
+                    <div>
+                        <Button onClick = {this.handleDelete}>删除</Button>
+                    </div>
+                    <Table
+                        bordered
+                        rowSelection = {rowCheckSelection}
                         dataSource = {this.state.dataSource2}
                         columns = {columns}
                         pagination = {false}
