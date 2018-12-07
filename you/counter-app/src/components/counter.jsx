7b638f50446c197109438719a2ad9e00e8b72c4e
changxianglin <281532612@qq.com>
2018-12-08 02:40:34 +0800

    add is js to create function include css so so

--- you/counter-app/src/components/counter.jsx
+++ you/counter-app/src/components/counter.jsx
@@ -4,7 +4,23 @@ class Counter extends Component {
     state = {
         count: 0,
         imageUrl: 'https://picsum.photos/200',
-        tags: ['tag1', 'tag2', 'tag3']
+        tags: []
+    }
+
+    constructor() {
+        super()
+        // console.log("Constructor", this) must have super()
+        this.handleIncrement = this.handleIncrement.bind(this)
+    }
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>
+
+        return <ul>{ this.state.tags.map(tag => <li key = { tag }>{ tag }</li>)}</ul>
+    }
+
+    handleIncrement() {
+        console.log('Increment Clicked', this)
     }
 
     render() { 
@@ -12,10 +28,9 @@ class Counter extends Component {
             <React.Fragment>
                 <img src={this.state.imageUrl} alt=""/>
                 <span className = {this.getBadgeClasses()}>{this.state.count}</span>
-                <button>Increment</button>
-                <ul>
-                    { this.state.tags.map(tag => <li key = { tag }>{ tag }</li>)}
-                </ul>
+                <button onClick = {this.handleIncrement}>Increment</button>
+                {this.state.tags.length === 0 && "Please create new tages!"}
+                { this.renderTags() }
             </React.Fragment>
         )
     }
