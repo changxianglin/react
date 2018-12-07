d0ef5a08c5c27c17ba81c0dd6202aae9227e3884
changxianglin <281532612@qq.com>
2018-12-07 23:59:59 +0800

    sorter

--- imoocmanage/src/pages/table/highTable.js
+++ imoocmanage/src/pages/table/highTable.js
@@ -265,6 +265,75 @@ export default class HighTable extends React.Component {
             }
         ]
 
+        const columns3 = [
+            {
+                title: "id",
+                dataIndex: "id",
+            },
+            {
+                title: "用户名",
+                dataIndex: "userName",
+            },
+            {
+                title: "性别",
+                dataIndex: "sex",
+                render(sex) {
+                    return sex == 1 ? '男' : '女'
+                }
+            },
+            {
+                title: "年龄",
+                key: "age",
+                dataIndex: "age",
+                sorter: (a, b) => {
+                    return a.age - b.age
+                }
+            },
+            {
+                title: "状态",
+                dataIndex: "state",
+                render(state) {
+                    let config = {
+                        "1": "咸鱼一条",
+                        "2": "风华浪子",
+                        "3": "北大才子",
+                        "4": "百度FE",
+                        "5": "创业者"
+                    }
+                    return config[state]
+                }
+            },
+            {
+                title: "爱好",
+                dataIndex: "interest",
+                render(interest) {
+                    let config = {
+                        "1": "游泳",
+                        "2": "打篮球",
+                        "3": "踢足球",
+                        "4": "跑步",
+                        "5": "爬山",
+                        "6": "骑行",
+                        "7": "桌球",
+                        "8": "麦霸",
+                    }
+                    return config[interest]
+                }
+            },
+            {
+                title: "生日",
+                dataIndex: "birthday",
+            },
+            {
+                title: "地址",
+                dataIndex: "address",
+            },
+            {
+                title: "早起时间",
+                dataIndex: "time",
+            }
+        ]
+
         return (
             <div>
                 <Card title = "头部固定">
@@ -283,6 +352,14 @@ export default class HighTable extends React.Component {
                         columns = {columns2}
                         pagination = {false}
                         scroll = {{x: 2760}}
+                    />
+                </Card>
+                <Card title = "表格排序" style = {{margin: "10px 0"}}>
+                    <Table
+                        bordered
+                        dataSource = {this.state.dataSource}
+                        columns = {columns3}
+                        pagination = {false}
                     />
                 </Card>
             </div>
