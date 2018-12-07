908d7406f31d91e0c74282c1d50d6c3cb72bea2f
changxianglin <281532612@qq.com>
2018-12-08 00:30:23 +0800

    bage compration

--- imoocmanage/src/pages/table/highTable.js
+++ imoocmanage/src/pages/table/highTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, Table, Modal, Button, message} from 'antd'
+import { Card, Table, Modal, Button, message, Badge} from 'antd'
 import axios from './../../axios/index'
 import Utils from './../../utils/utils';
 
@@ -19,7 +19,7 @@ export default class HighTable extends React.Component {
     request = () => {
         let _this = this
         axios.ajax({
-            url: "/table/list",
+            url: "/table/high/list",
             data: {
                 params: {
                     page: this.param.page
@@ -33,6 +33,25 @@ export default class HighTable extends React.Component {
                 this.setState({
                     dataSource: res.result.list
                 })
+            }
+        })
+    }
+
+    handleChange = (pagination, filters, sorter) => {
+        console.log(sorter)
+        this.setState({
+            sortOder: sorter.sortOder
+        })
+    }
+
+    handleDelete = (item) => {
+        let id = item.id
+        Modal.confirm({
+            title: '确认',
+            content: '您确认要删除此条数据吗？',
+            onOk: () => {
+                message.success('删除成功')
+                this.request()
             }
         })
     }
@@ -283,11 +302,11 @@ export default class HighTable extends React.Component {
             },
             {
                 title: "年龄",
-                key: "age",
                 dataIndex: "age",
                 sorter: (a, b) => {
                     return a.age - b.age
-                }
+                },
+                sortOder: this.state.sortOder
             },
             {
                 title: "状态",
@@ -334,6 +353,74 @@ export default class HighTable extends React.Component {
             }
         ]
 
+        const columns4 = [
+            {
+                title: "id",
+                dataIndex: "id",
+            },
+            {
+                title: "用户名",
+                dataIndex: "userName",
+            },
+            {
+                title: "性别",
+                dataIndex: "sex",
+                render(sex) {
+                    return sex == 1 ? '男' : '女'
+                }
+            },
+            {
+                title: "年龄",
+                dataIndex: "age",
+                sorter: (a, b) => {
+                    return a.age - b.age
+                },
+                sortOder: this.state.sortOder
+            },
+            {
+                title: "状态",
+                dataIndex: "state",
+                render(state) {
+                    let config = {
+                        "1": "咸鱼一条",
+                        "2": "风华浪子",
+                        "3": "北大才子",
+                        "4": "百度FE",
+                        "5": "创业者"
+                    }
+                    return config[state]
+                }
+            },
+            {
+                title: "爱好",
+                dataIndex: "interest",
+                render(interest) {
+                    let config = {
+                        "1": <Badge status="sucess" text = "成功" />,
+                        "2": <Badge status="error" text = "保存" />,
+                        "3": <Badge status="default" text = "正常" />,
+                        "4": <Badge status="processing" text = "进行中" />,
+                        "5": <Badge status="warning" text = "警告" />
+                    }
+                    return config[interest]
+                }
+            },
+            {
+                title: "生日",
+                dataIndex: "birthday",
+            },
+            {
+                title: "地址",
+                dataIndex: "address",
+            },
+            {
+                title: "操作",
+                render: (text, item) => {
+                    return <Button size = "small" onClick = {(item) => {this.handleDelete(item)}}>删除</Button>
+                }
+            }
+        ]
+
         return (
             <div>
                 <Card title = "头部固定">
@@ -359,6 +446,15 @@ export default class HighTable extends React.Component {
                         bordered
                         dataSource = {this.state.dataSource}
                         columns = {columns3}
+                        pagination = {false}
+                        onChange = {this.handleChange}
+                    />
+                </Card>
+                <Card title = "操作按钮" style = {{margin: "10px 0"}}>
+                    <Table
+                        bordered
+                        dataSource = {this.state.dataSource}
+                        columns = {columns4}
                         pagination = {false}
                     />
                 </Card>
