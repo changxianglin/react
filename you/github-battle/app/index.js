dbd48cffa767cf53f921f9c58dacf8c3bed3af7d
changxianglin <281532612@qq.com>
2018-12-11 02:25:08 +0800

    base can work

--- you/github-battle/app/index.js
+++ you/github-battle/app/index.js
@@ -7,6 +7,7 @@ class App extends React.Component {
     return (
       <div>
         Hello React
+        <p> can use es6</p>
       </div>
     )
   }
---
+++ you/github-battle/config/webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use:[
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ],
+  devServer: {
+    host: '0.0.0.0',
+    port: '9099',
+    open: true,
+    historyApiFallback: true,
+  }
+}
