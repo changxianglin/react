e2f7495d86e7e3542abd0318feed24819e664e36
changxianglin <281532612@qq.com>
2018-12-20 01:54:31 +0800

    redux delete

--- you/todolist/src/TodoList.js
+++ you/todolist/src/TodoList.js
@@ -28,7 +28,7 @@ class TodoList extends Component {
                     style = {{width: '300px', marginTop: '10px'}} 
                     bordered
                     dataSource = {this.state.list}
-                    renderItem = {item => (<List.Item>{item}</List.Item>)}
+                    renderItem = {(item, index) => (<List.Item onClick = {this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
                  />   
             </div>
         )
@@ -49,6 +49,14 @@ class TodoList extends Component {
     handleBtnClick() {
         const action = {
             type: 'add_todo_item'
+        }
+        store.dispatch(action)
+    }
+
+    handleItemDelete(index) {
+        const action = {
+            type: 'delete_todo_item',
+            index
         }
         store.dispatch(action)
     }
--- you/todolist/src/store/reducer.js
+++ you/todolist/src/store/reducer.js
@@ -16,5 +16,10 @@ export default (state = defaultState, action) => {
         newState.inputValue = ''
         return newState
     }
+    if(action.type === 'delete_todo_item') {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.index, 1)
+        return newState
+    }
     return state
 }
