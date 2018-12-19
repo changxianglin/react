9720f5fbda1dfcf8a19c3e7bf59c121ecf9feb49
changxianglin <281532612@qq.com>
2018-12-20 01:15:59 +0800

    creat redux store reducer

--- you/todolist/src/TodoList.js
+++ you/todolist/src/TodoList.js
@@ -1,27 +1,24 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
-
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
+import store from './store'
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props)
+        this.state = store.getState()
+    }
     render() {
         return (
             <div style = {{ marginTop: '10px', marginLeft: '10px'}}>
                 <div>
-                    <Input placeholder = "todo info" style = {{ width: '300px', marginRight: '10px'}} />
+                    <Input value = {this.state.inputValue} placeholder = "todo info" style = {{ width: '300px', marginRight: '10px'}} />
                     <Button type = 'primary' >提交</Button>
                  </div>
                  <List
                     style = {{width: '300px', marginTop: '10px'}} 
                     bordered
-                    dataSource = {data}
+                    dataSource = {this.state.list}
                     renderItem = {item => (<List.Item>{item}</List.Item>)}
                  />   
             </div>
