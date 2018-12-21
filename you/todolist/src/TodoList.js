966f23ea151d2a51e682505bed8c403686e7d489
changxianglin <281532612@qq.com>
2018-12-22 02:36:16 +0800

    use react-redux redux wirte todolist component

--- you/todolist/src/TodoList.js
+++ you/todolist/src/TodoList.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import  { connect } from 'react-redux'
 
-class TodoList extends Component {
-    render() {
+const TodoList = (props) => {
+    const {inputValue, list, handleInputChange, handleClick, handleDelete } = props
         return (
             <div>
                 <div>
                     <input 
-                    placeholder = {this.props.inputValue}
-                    onChange = {this.props.handleInputChange}
+                    value = {inputValue}
+                    onChange = {handleInputChange}
                      />
-                    <button>提交</button>
+                    <button 
+                    onClick = {handleClick}>
+                    提交</button>
                 </div>
                 <ul>
-                    <li>zhang</li>
+                    {
+                        list.map((item, index) => {
+                            return (
+                                <li 
+                                onClick = {() => {handleDelete(index)}}
+                                key = {index}>{item}</li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
         )
-    }
 }
 
 const mapStateToProps = (state) => {
     return {
-        inputValue: state.inputValue
+        inputValue: state.inputValue,
+        list: state.list
     }
 }
 
@@ -32,6 +42,19 @@ const mapDispathToProps = (dispatch) => {
             const action = {
                 type: 'change_input_value',
                 value: e.target.value
+            }
+            dispatch(action)
+        },
+        handleClick() {
+            const action = {
+                type: 'add_item'
+            }
+            dispatch(action)
+        },
+        handleDelete(index) {
+            const action = {
+                type: 'delete_item',
+                value: index
             }
             dispatch(action)
         }
--- you/todolist/src/store/reducer.js
+++ you/todolist/src/store/reducer.js
@@ -1,5 +1,5 @@
 const defaultState = {
-    inputValue: 'hello zhangsan',
+    inputValue: '',
     list: []
 }
 
@@ -7,6 +7,19 @@ export default (state = defaultState, action) => {
     if(action.type === 'change_input_value') {
         const newState = JSON.parse(JSON.stringify(state))
         newState.inputValue = action.value
+        return newState
+    }
+    
+    if(action.type === 'add_item') {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.push(newState.inputValue)
+        newState.inputValue = ''
+        return newState
+    }
+    
+    if(action.type === 'delete_item') {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.list.splice(action.value, 1)
         return newState
     }
     return state
