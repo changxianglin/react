d5f32290a067bcee9739e359119d15f193833c3a
changxianglin <281532612@qq.com>
2018-12-21 23:59:44 +0800

    redux-thunk

--- you/todolist/src/store/index.js
+++ you/todolist/src/store/index.js
@@ -1,9 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducer from './reducer'
+import thunk from 'redux-thunk'
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    applyMiddleware(thunk),
+)
 
 export default store
