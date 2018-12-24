bfc2d3d96c46d00576bdd79dc438e498a84aa047
changxianglin <281532612@qq.com>
2018-12-25 00:22:01 +0800

    change value

--- you/testrweiui/src/App.js
+++ you/testrweiui/src/App.js
@@ -9,11 +9,28 @@ import axios from './axios'
 import { test, todo, have, end } from './service'
 
 class App extends Component {
+
+  state = {
+    test: '',
+    todo: '',
+    have: {
+      id: '',
+      name: '',
+      age: '',
+      gender: '',
+    },
+    end: '',
+  }
+
   handleTest = () => {
     axios.ajax({
       url: test,
     }).then(res => {
-      console.log(res.data)
+      if(res.code == 0) {
+        this.setState({
+          test: res.data.msg
+        })
+      }
     })
   }
 
@@ -21,7 +38,11 @@ class App extends Component {
     axios.ajax({
       url: todo
     }).then(res => {
-      console.log(res.data)
+      if(res.code == 0) {
+        this.setState({
+          todo: res.data.msg
+        })
+      }
     })
   }
 
@@ -29,7 +50,12 @@ class App extends Component {
     axios.ajax({
       url: have,
     }).then(res => {
-      console.log(res.data)
+      if(res.code == 0) {
+        console.log(res.data)
+        this.setState({
+          have: res.data
+        })
+      }
     })
   }
 
@@ -37,7 +63,11 @@ class App extends Component {
     axios.ajax({
       url: end
     }).then(res => {
-      console.log(res.data)
+      if(res.code == 0) {
+        this.setState({
+          end: res.data.msg
+        })
+      }
     })
   }
 
@@ -52,6 +82,11 @@ class App extends Component {
         <Button onClick = { this.handleTodo }>Todo</Button>
         <Button onClick = { this.handleHave }>Have</Button>
         <Button onClick = { this.handleEnd }>End</Button>
+        <hr />
+        <span>{this.state.test}</span>
+        <span>{this.state.todo}</span>
+        <span>{this.state.have.name}</span>
+        <span>{this.state.end}</span>
       </div>
     );
   }
