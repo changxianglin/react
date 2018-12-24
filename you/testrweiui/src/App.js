cd4c43d1f5c8dc8342b991ec70a5b7f2ec7d7b6b
changxianglin <281532612@qq.com>
2018-12-25 01:42:41 +0800

    add some images and use build and create server to run

--- you/testrweiui/src/App.js
+++ you/testrweiui/src/App.js
@@ -8,9 +8,17 @@ import 'react-weui/build/packages/react-weui.css';
 import axios from './axios'
 import { test, todo, have, end } from './service'
 
+const test1 = require('./img/1.jpg')
+const test2 = require('./img/2.jpg')
+const test3 = require('./img/3.jpg')
+
 class App extends Component {
 
   state = {
+    defTest: 'test',
+    defTodo: 'tod0',
+    defHave: 'have',
+    defEnd: 'end',
     test: '',
     todo: '',
     have: {
@@ -24,7 +32,7 @@ class App extends Component {
 
   handleTest = () => {
     axios.ajax({
-      url: test,
+      url: test + `?prams=${this.state.defTest}`,
     }).then(res => {
       if(res.code == 0) {
         this.setState({
@@ -36,7 +44,7 @@ class App extends Component {
 
   handleTodo = () => {
     axios.ajax({
-      url: todo
+      url: todo + `?todo=${this.state.defTodo}`
     }).then(res => {
       if(res.code == 0) {
         this.setState({
@@ -48,7 +56,7 @@ class App extends Component {
 
   handleHave = () => {
     axios.ajax({
-      url: have,
+      url: have + `?have=${this.state.defHave}`,
     }).then(res => {
       if(res.code == 0) {
         console.log(res.data)
@@ -61,7 +69,7 @@ class App extends Component {
 
   handleEnd = () => {
     axios.ajax({
-      url: end
+      url: end + `?end=${this.state.defEnd}`
     }).then(res => {
       if(res.code == 0) {
         this.setState({
@@ -87,6 +95,10 @@ class App extends Component {
         <span>{this.state.todo}</span>
         <span>{this.state.have.name}</span>
         <span>{this.state.end}</span>
+        <hr />
+        <img src = {test1} alt = "" />
+        <img src = {test2} alt = "" />
+        <img src = {test3} alt = "" />
       </div>
     );
   }
