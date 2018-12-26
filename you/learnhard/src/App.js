6f2f10d1c38679dc988ec56715a5d3fdc08b46d8
changxianglin <281532612@qq.com>
2018-12-26 23:59:40 +0800

    entry

--- you/learnhard/src/App.js
+++ you/learnhard/src/App.js
@@ -7,13 +7,12 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="http://zhourong.club"
             target="_blank"
             rel="noopener noreferrer"
           >
