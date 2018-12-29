8c8ba8f067e7e5311bc8b771883f364f02f14fde
changxianglin <281532612@qq.com>
2018-12-29 22:28:39 +0800

    render menu

--- you/relearnimooc/src/components/NavLeft/index.js
+++ you/relearnimooc/src/components/NavLeft/index.js
@@ -9,6 +9,27 @@ const MenuItemGroup = Menu.ItemGroup;
 
 
 export default class NavLeft extends React.Component{
+    componentWillMount() {
+        const menuTreeNode = this.renderMenu(MenuConfig)
+        this.setState({
+            menuTreeNode
+        })
+    }
+
+    // 菜单渲染
+    renderMenu = (data) => {
+        return data.map((item) => {
+            if(item.children) {
+                return (
+                    <SubMenu title = {item.title} key = {item.key}>
+                        { this.renderMenu(item.children) }
+                    </SubMenu>
+                )
+            }
+            return <Menu.Item title = {item.title} key = {item.key}>{item.title}</Menu.Item>
+        })
+    }
+
     render() {
         return (
             <div>
@@ -19,12 +40,7 @@ export default class NavLeft extends React.Component{
                 <Menu
                     theme = "dark"
                 >
-                    <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-                            <Menu.Item key="1">Option 1</Menu.Item>
-                            <Menu.Item key="2">Option 2</Menu.Item>
-                            <Menu.Item key="3">Option 3</Menu.Item>
-                            <Menu.Item key="4">Option 4</Menu.Item>
-                    </SubMenu>
+                    { this.state.menuTreeNode  }
                 </Menu>
             </div>
         )
