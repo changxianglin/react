ba38b8b0318367bb72503b28dfe161b4096e729c
changxianglin <281532612@qq.com>
2018-12-29 21:32:20 +0800

    menu and style Grid

--- you/relearnimooc/src/admin.js
+++ you/relearnimooc/src/admin.js
@@ -2,24 +2,23 @@ import React from 'react'
 import { Row, Col } from 'antd';
 import Header from './components/Header'
 import Footer from './components/Footer'
+import NavLeft from './components/NavLeft'
+import './style/common.less'
 
 export default class Admin extends React.Component{
     render() {
         return (
-            <Row>
-                <Col span = {3}>
-                    Left
+            <Row className = "container">
+                <Col span = {4} className = "nav-left">
+                    <NavLeft />
                 </Col>
-                <Col span = {21}>
-                    <Header>
-                        Header
-                    </Header>
-                    <Row>
-                        Content
+                <Col span = {20} className = "main">
+                    <Header />
+                    <Row className = "content">
+                        content
+                        {/* { this.props.children } */}
                     </Row>
-                    <Footer>
-                        Footer
-                    </Footer>
+                    <Footer />
                 </Col>
             </Row>
         )
