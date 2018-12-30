a7dfcc7960ec7cd9ebcd021c5d0981850eae4023
changxianglin <281532612@qq.com>
2018-12-30 23:11:56 +0800

    init compnents

--- you/doc/src/index.js
+++ you/doc/src/index.js
@@ -2,17 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
+// function tick() {
+//     const element = (
+//       <div>
+//         <h1>Hello, world!</h1>
+//         <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//       </div>
+//     );
+//     ReactDOM.render(
+//       element,
+//       document.getElementById('root')
+//     );
+//   }
+// setInterval(tick, 1000);
+
+function Clock(props) {
+    return (
+        <div>
+            <h1>Hello world!</h1>
+            <h2>It is { props.date.toLocaleTimeString() }.</h2>
+        </div>
+    )
+}
+
 function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
     ReactDOM.render(
-      element,
-      document.getElementById('root')
-    );
-  }
+        <Clock date = {new Date()} />,
+        document.getElementById('root')
+    )
+}
+
+setInterval(tick, 1000)
   
-  setInterval(tick, 1000);
+  
