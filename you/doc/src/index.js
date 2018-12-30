1b2d4d900c9928453695123824dbd327477be78e
changxianglin <281532612@qq.com>
2018-12-30 23:16:45 +0800

    class component

--- you/doc/src/index.js
+++ you/doc/src/index.js
@@ -16,13 +16,33 @@ import ReactDOM from 'react-dom'
 //   }
 // setInterval(tick, 1000);
 
-function Clock(props) {
-    return (
-        <div>
-            <h1>Hello world!</h1>
-            <h2>It is { props.date.toLocaleTimeString() }.</h2>
-        </div>
-    )
+// function Clock(props) {
+//     return (
+//         <div>
+//             <h1>Hello world!</h1>
+//             <h2>It is { props.date.toLocaleTimeString() }.</h2>
+//         </div>
+//     )
+// }
+
+// function tick() {
+//     ReactDOM.render(
+//         <Clock date = {new Date()} />,
+//         document.getElementById('root')
+//     )
+// }
+
+// setInterval(tick, 1000)
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world!</h1>
+                <h2>It is { this.props.date.toLocaleTimeString() }.</h2>
+            </div>
+        )
+    }
 }
 
 function tick() {
