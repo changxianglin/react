372013dabd5f0738d8e39362a57e7ab1f05253e0
changxianglin <281532612@qq.com>
2018-12-30 23:42:58 +0800

    component isolated

--- you/doc/src/index.js
+++ you/doc/src/index.js
@@ -55,6 +55,44 @@ import ReactDOM from 'react-dom'
 // setInterval(tick, 1000)
 
 
+// class Clock extends React.Component {
+//     constructor (props) {
+//         super(props)
+//         this.state = {date: new Date()}
+//     }
+
+//     componentDidMount() {
+//         this.timerID = setInterval(
+//             () => this.tick(),
+//             1000
+//         )
+//     }
+
+//     componentWillUnmount() {
+//         clearInterval(this.timerID)
+//     }
+
+//     tick () {
+//         this.setState({
+//             date: new Date()
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Hello world!</h1>
+//                 <h2>It is { this.state.date.toLocaleTimeString() }.</h2>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <Clock  />,
+//     document.getElementById('root')
+// )
+
 class Clock extends React.Component {
     constructor (props) {
         super(props)
@@ -88,9 +126,19 @@ class Clock extends React.Component {
     }
 }
 
-ReactDOM.render(
-    <Clock  />,
+function App() {
+    return (
+      <div>
+        <Clock />
+        <Clock />
+        <Clock />
+      </div>
+    );
+  }
+  
+  ReactDOM.render(
+    <App />,
     document.getElementById('root')
-)
+  );
   
   
