b1aeb0bfabfe8f29ba38db3826df1f3ca71e8263
changxianglin <281532612@qq.com>
2018-12-31 20:17:21 +0800

    change state

--- you/doc/src/index.js
+++ you/doc/src/index.js
@@ -1,23 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component{
-    handleClick = (e) => {
-        e.preventDefault()
-        console.log('The links ')
+// class App extends React.Component{
+//     handleClick = (e) => {
+//         e.preventDefault()
+//         console.log('The links ')
+//     }
+//     render() {
+//         return (
+//             <div>
+//             <a href="#" onClick={this.handleClick}>
+//                 Click me
+//             </a>
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// )
+
+
+class Toggle extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {isToggleOn: true}
+        this.handleClick = this.handleClick.bind(this)
     }
+
+    handleClick() {
+        this.setState(
+            preState => ({
+                isToggleOn: !this.state.isToggleOn
+            })
+        )
+    }
+
     render() {
         return (
             <div>
-            <a href="#" onClick={this.handleClick}>
-                Click me
-            </a>
+                <button onClick = {this.handleClick}>
+                    {this.state.isToggleOn ? 'NO' : 'OFF'}
+                </button>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <Toggle />,
     document.getElementById('root')
 )
