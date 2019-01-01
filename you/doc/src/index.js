f11945ed51feb96368ea455f5d0c980b167c21bd
changxianglin <281532612@qq.com>
2019-01-01 20:14:46 +0800

    textarea submit

--- you/doc/src/index.js
+++ you/doc/src/index.js
@@ -1,10 +1,53 @@
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// class NameFrom extends React.Component{
+//     constructor(props) {
+//         super(props)
+//         this.state = { value: '' }
+
+//         this.handleChange = this.handleChange.bind(this)
+//         this.handleSubmit = this.handleSubmit.bind(this)
+//     }
+
+//     handleChange(event) {
+//         this.setState({
+//             value: event.target.value
+//         })
+//     }
+
+//     handleSubmit(event) {
+//         alert('A name was submitted: ' + this.state.value)
+//         event.preventDefault()
+//     }
+
+//     render() {
+//         return (
+//             <form onSubmit = {this.handleSubmit}>
+//                 <label>
+//                     Name:
+//                     <input type = 'text' value = {this.state.value} onChange = {this.handleChange} />
+//                 </label>
+//                 <input type = 'submit' value = 'Submit' />
+//             </form>
+//         )
+//     }
+// }
+
+// ReactDOM.render(
+//     <NameFrom />,
+//     document.getElementById('root')
+// )
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class NameFrom extends React.Component{
-    constructor(props) {
+class EassyForm extends React.Component{
+    constructor(props){
         super(props)
-        this.state = { value: '' }
+        this.state = {
+            value: 'Please wirte an essay about your favorite DOM element.'
+        }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -17,7 +60,7 @@ class NameFrom extends React.Component{
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value)
+        alert('An essay was submitted: ' + this.state.value)
         event.preventDefault()
     }
 
@@ -26,15 +69,15 @@ class NameFrom extends React.Component{
             <form onSubmit = {this.handleSubmit}>
                 <label>
                     Name:
-                    <input type = 'text' value = {this.state.value} onChange = {this.handleChange} />
+                    <textarea value = {this.state.value} onChange = { this.handleChange } />
                 </label>
-                <input type = 'submit' value = 'Submit' />
+                <input type = 'submit' value = 'submit'/>
             </form>
         )
     }
 }
 
 ReactDOM.render(
-    <NameFrom />,
+    <EassyForm />,
     document.getElementById('root')
 )
