668b05a40580d7e85d16b666f4c512ca5f0961d7
changxianglin <281532612@qq.com>
2019-01-05 23:59:57 +0800

    redux action disptch

--- you/todoredux/src/TodoList.js
+++ you/todoredux/src/TodoList.js
@@ -8,11 +8,18 @@ class TodoList extends Component{
     constructor(props) {
         super(props)
         this.state = store.getState()
+
+        this.handleInputChange = this.handleInputChange.bind(this)
     }
     render() {
         return (
             <div style = {{marginTop: '10px', marginLeft: '10px'}}>
-                <Input value = {this.state.inputValue} placeholder = "todo info" style = {{width: '300px', marginRight: '10px'}} />
+                <Input 
+                value = {this.state.inputValue} 
+                placeholder = "todo info" 
+                style = {{width: '300px', marginRight: '10px'}}
+                onChange = {this.handleInputChange}
+                 />
                 <Button type = "primary">提交</Button>
                 <List
                 style = {{ marginTop: '10px', width: '300px'}}
@@ -22,6 +29,14 @@ class TodoList extends Component{
                 />
             </div>
         )
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action)
     }
 }
 
--- you/todoredux/src/store/reducer.js
+++ you/todoredux/src/store/reducer.js
@@ -4,5 +4,10 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+    if(action.type === 'change_input_value') {
+        const newState = JSON.parse(JSON.stringify(state))
+        newState.inputValue = action.value
+    }
+    console.log(state, action)
     return state
 }
