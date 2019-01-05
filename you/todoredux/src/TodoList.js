ce493e05d40792bf78e7aadc45afd75910790ec2
changxianglin <281532612@qq.com>
2019-01-06 00:36:03 +0800

    use actionTypes

--- you/todoredux/src/TodoList.js
+++ you/todoredux/src/TodoList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
 import store from './store'
+import { CHANGE_INPUT_VALUE, ADD_TODO_ITEM, DELETE_TODO_ITEM} from './store/actionTypes'
 
 
 class TodoList extends Component{
@@ -46,7 +47,7 @@ class TodoList extends Component{
 
     handleInputChange(e) {
         const action = {
-            type: 'change_input_value',
+            type: CHANGE_INPUT_VALUE,
             value: e.target.value
         }
         store.dispatch(action)
@@ -58,14 +59,14 @@ class TodoList extends Component{
 
     handleBtnClick() {
         const action = {
-            type: 'add_todo_item'
+            type: ADD_TODO_ITEM
         }
         store.dispatch(action)
     }
 
     handleItemDelete(index) {
         const action = {
-            type: 'delete_todo_item',
+            type: DELETE_TODO_ITEM,
             index
         }
         store.dispatch(action)
