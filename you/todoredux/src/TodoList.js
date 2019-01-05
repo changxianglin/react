d2a1035fee6bd315fd7eebe4306bf90ec38cac1f
changxianglin <281532612@qq.com>
2019-01-05 23:46:14 +0800

    use redux save data

--- you/todoredux/src/TodoList.js
+++ you/todoredux/src/TodoList.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react'
 import 'antd/dist/antd.css'
 import { Input, Button, List } from 'antd'
-
-const data = []
+import store from './store'
 
 
 class TodoList extends Component{
+    constructor(props) {
+        super(props)
+        this.state = store.getState()
+    }
     render() {
         return (
             <div style = {{marginTop: '10px', marginLeft: '10px'}}>
-                <Input placeholder = "todo info" style = {{width: '300px', marginRight: '10px'}} />
+                <Input value = {this.state.inputValue} placeholder = "todo info" style = {{width: '300px', marginRight: '10px'}} />
                 <Button type = "primary">提交</Button>
                 <List
                 style = {{ marginTop: '10px', width: '300px'}}
                 bordered
-                dataSource = {data}
+                dataSource = {this.state.list}
                 renderItem = {item => (<List.Item>{item}</List.Item>)}
                 />
             </div>
