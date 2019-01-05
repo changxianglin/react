8391d45862104b53f8570923ee9c6be390c9f971
changxianglin <281532612@qq.com>
2019-01-06 01:41:27 +0800

    no state component

--- you/todoredux/src/TodoListUI.js
+++ you/todoredux/src/TodoListUI.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Input, Button, List } from 'antd'
 
-class TodoListUI extends Component{
-    render() {
-        return (
-            <div style = {{marginTop: '10px', marginLeft: '10px'}}>
+const TodoListUI = (props) => {
+    return (
+        <div style = {{marginTop: '10px', marginLeft: '10px'}}>
             <Input 
-            value = {this.props.inputValue} 
+            value = {props.inputValue} 
             placeholder = "todo info" 
             style = {{width: '300px', marginRight: '10px'}}
-            onChange = {this.props.handleInputChange}
+            onChange = {props.handleInputChange}
              />
             <Button 
             type = "primary"
-            onClick = {this.props.handleBtnClick}
+            onClick = {props.handleBtnClick}
             >提交</Button>
             <List
             style = {{ marginTop: '10px', width: '300px'}}
             bordered
-            dataSource = {this.props.list}
-            renderItem = {(item, index) => (<List.Item onClick = {(index) => {this.props.handleItemDelete(index)}}>{item}</List.Item>)}
+            dataSource = {props.list}
+            renderItem = {(item, index) => (<List.Item onClick = {() => {props.handleItemDelete(index)}}>{item}</List.Item>)}
             />
         </div>
-        )
-    }
+    )
 }
+
 
 export default TodoListUI
