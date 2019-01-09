c45453def5e33d65f1b4c81ba7bfc566bcf728e1
changxianglin <281532612@qq.com>
2019-01-09 23:51:43 +0800

    test react use svg

---
+++ you/useantd/src/svg.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class TestSVG extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="ticket-map">
+                    <div className="svg-box">
+                            asfd
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TestSVG
