f1761676fcb58a9e33c073134dd4f3835b6311fd
changxianglin <281532612@qq.com>
2019-01-11 02:40:30 +0800

    css animition ues react-transition-group

--- jianshu2/src/common/header/index.js
+++ jianshu2/src/common/header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { 
     HeaderWrapper,
     Logo,
@@ -11,6 +12,15 @@ import {
  } from './style.js'
 
 class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            focused: false,
+        }
+
+        this.handleInputFocus = this.handleInputFocus.bind(this)
+        this.handleInputBlur = this.handleInputBlur.bind(this)
+    }
     render() {
         return (
             <HeaderWrapper>
@@ -23,8 +33,18 @@ class Header extends Component {
                         <i className = 'iconfont'>&#xe636;</i>
                     </NavItem>
                     <SearchWrapper>
-                        <NavSearch></NavSearch>
-                        <i className = 'iconfont'>&#xe62b;</i>
+                        <CSSTransition
+                            in = {this.state.focused}
+                            timeout = {200}
+                            classNames = "slide"
+                        >
+                            <NavSearch
+                                className = {this.state.focused ? 'focused' : ''}
+                                onFocus = {this.handleInputFocus}
+                                onBlur = {this.handleInputBlur}
+                            ></NavSearch>
+                        </CSSTransition>
+                        <i className = {this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe62b;</i>
                     </SearchWrapper>
                 </Nav>
                 <Addition>
@@ -36,6 +56,18 @@ class Header extends Component {
                 </Addition>
             </HeaderWrapper>
         )
+    }
+
+    handleInputFocus() {
+        this.setState({
+            focused: true
+        })
+    }
+
+    handleInputBlur() {
+        this.setState({
+            focused: false
+        })
     }
 }
 
--- jianshu2/src/common/header/style.js
+++ jianshu2/src/common/header/style.js
@@ -47,6 +47,18 @@ export const NavItem = styled.div`
 export const SearchWrapper = styled.div`
     position: relative; 
     float: left;
+    .slide-enter {
+        transition: all .2s ease-out;
+    }
+    .slide-enter-active {
+        width: 240px;
+    }
+    .slide-exit {
+        transition: all .2s ease-out;
+    }
+    .slide-exit-active {
+        width: 160px;
+    }
     .iconfont {
         position: absolute;
         width: 30px;
@@ -55,6 +67,10 @@ export const SearchWrapper = styled.div`
         right: 5px;
         bottom: 5px;
         text-align: center;
+        &.focused {
+            background: #777;
+            color: #fff;
+        }
     }
 `
 
@@ -63,7 +79,7 @@ export const NavSearch = styled.input.attrs({
 })`
     width: 160px;
     height: 38px;
-    padding: 0 20px;
+    padding: 0 30px 0 20px;
     margin-top: 9px;
     margin-left: 20px;
     box-sizing: border-box;
@@ -72,8 +88,12 @@ export const NavSearch = styled.input.attrs({
     border-radius: 19px;
     background: #eee;
     font-size: 14px;
+    color: #666;
     &::placeholder {
         color: #999;
+    }
+    &.focused {
+        width: 240px;
     }
 `
 
