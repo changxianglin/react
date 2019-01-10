6c2305bf7796cb3137cad85f5c0006fad58910d3
changxianglin <281532612@qq.com>
2019-01-11 02:16:13 +0800

    oo

--- you/useantd/src/todo.js
+++ you/useantd/src/todo.js
@@ -3,17 +3,35 @@ import React from 'react'
 class Todo extends React.Component{
 
     state = {
-        item: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        item: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+        item2: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        
+        item3: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     }
 
     render() {
         return (
             <div>
+
                 {
-                    this.state.item.map(item => {
-                        return <div>{item}</div>
+                    this.state.item.map((item, index) => {
+                        return <div key = {index}>{item}</div>
                     })
                 }
+
+                {
+                    this.state.item2.map((item, index) => {
+                        return <span key = {index}>{item}</span>
+                    })
+                }
+
+                {
+                    this.state.item3.map((item, index) => {
+                        return <div key = {index}>{item}</div>
+                    })
+                }
+
             </div>
         )
     }
