18a0fd82862bde1aad8481cc1db2a61e74d30a35
changxianglin <281532612@qq.com>
2019-01-12 01:16:03 +0800

    actionCreators constants

--- jianshu2/src/common/header/index.js
+++ jianshu2/src/common/header/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
+import { actionCreators } from './store'
+
 import { 
     HeaderWrapper,
     Logo,
@@ -58,16 +60,10 @@ const  mapStateToPorps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         handleInputFocus() {
-            const action = {
-                type: 'search_fous',
-            }
-            dispatch(action)
+            dispatch(actionCreators.searchFocus())
         },
         handleInputBlur() {
-            const action = {
-                type: 'search_blur'
-            }
-            dispatch(action)
+            dispatch(actionCreators.searchBlur())
         }
     }
 }
