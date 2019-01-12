df2eb01bc7dce4d273b460bcf358f85720654493
changxianglin <281532612@qq.com>
2019-01-12 17:00:55 +0800

    styleout

--- jianshu2/src/common/header/index.js
+++ jianshu2/src/common/header/index.js
@@ -11,8 +11,38 @@ import {
     NavSearch,
     Addition,
     Button,
+    SearchInfo,
+    SearchInfoTitle,
+    SearchInfoSwitch,
+    SearchInfoList,
+    SearchInfoItem,
     SearchWrapper,
  } from './style.js'
+
+ const getListArea = (show) => {
+    if(show) {
+        return (
+            <SearchInfo>
+            <SearchInfoTitle>
+                热门搜索    
+                <SearchInfoSwitch>换一批</SearchInfoSwitch>
+            </SearchInfoTitle>
+            <SearchInfoList>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+                <SearchInfoItem>教育</SearchInfoItem>
+            </SearchInfoList>
+        </SearchInfo>
+        )
+    } else {
+        return null 
+    }
+ }
 
 const Header = (props) => {
     return (
@@ -38,6 +68,9 @@ const Header = (props) => {
                             ></NavSearch>
                         </CSSTransition>
                         <i className = {props.focused ? 'focused iconfont' : 'iconfont'}>&#xe62b;</i>
+                           {
+                               getListArea(props.focused)
+                           } 
                     </SearchWrapper>
                 </Nav>
                 <Addition>
