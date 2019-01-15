9fadae8a14240c0af09ddaea428a9f54d9436c05
changxianglin <281532612@qq.com>
2019-01-16 02:25:58 +0800

    use store

--- jianshu2/src/store/reducer.js
+++ jianshu2/src/store/reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux-immutable'
 
 import { reducer as HeaderReducer } from '../common/header/store'
 import { reducer as HomeReducer }  from '../pages/home/store'
+import { reducer as DetailReducer } from '../pages/detail/store'
 
 const reducer =  combineReducers({
     header: HeaderReducer,
     home: HomeReducer,
+    detail: DetailReducer,
 })
 
 export default reducer
