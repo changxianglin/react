87d8ae10d95ecfdd8e1196e28358aad48a385338
changxianglin <281532612@qq.com>
2019-01-18 01:28:05 +0800

    login

--- jianshu2/src/common/header/index.js
+++ jianshu2/src/common/header/index.js
@@ -2,6 +2,7 @@ import React, { Component} from 'react'
 import { connect } from 'react-redux'
 import { CSSTransition } from 'react-transition-group'
 import { actionCreators } from './store'
+import { actionCreators as loginActionCreators} from '../../pages/login/store'
 
 import { 
     HeaderWrapper,
@@ -60,7 +61,7 @@ import {
     }
 
      render() {
-        const { focused, handleInputFocus, handleInputBlur, list } = this.props
+        const { focused, login, logout, handleInputFocus, handleInputBlur, list } = this.props
         return (
             <HeaderWrapper>
                 <Link to = '/'>
@@ -69,7 +70,13 @@ import {
                     <Nav>
                         <NavItem className = 'left active'>首页</NavItem>
                         <NavItem className = 'left'>下载APP</NavItem>
-                        <NavItem className = 'right'>登录</NavItem>
+                        {
+                            login ? 
+                            <NavItem onClick = {logout} className = 'right'>退出</NavItem> : 
+                            <Link to = '/login'>
+                                <NavItem className = 'right'>登录</NavItem>
+                            </Link>
+                        }
                         <NavItem className = 'right'>
                             <i className = 'iconfont'>&#xe636;</i>
                         </NavItem>
@@ -110,6 +117,7 @@ const  mapStateToPorps = (state) => {
         page: state.getIn(['header', 'page']),
         totalPage: state.getIn(['header', 'totalPage']),
         mouseIn: state.getIn(['header', 'mouseIn']),
+        login: state.getIn(['login', 'login']),
     }
 }
 
@@ -143,6 +151,9 @@ const mapDispatchToProps = (dispatch) => {
             } else {
                  dispatch(actionCreators.changePage(1))
             }
+        },
+        logout() {
+            dispatch(loginActionCreators.logout())
         }
     }
 }
--- jianshu2/src/pages/login/index.js
+++ jianshu2/src/pages/login/index.js
@@ -1,5 +1,7 @@
 import React, { PureComponent } from 'react'
+import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { actionCreators } from './store'
 import {
     LoginWrapper,
     LoginBox,
@@ -9,24 +11,31 @@ import {
 
 class Login extends PureComponent {
     render() {
-        return (
-            <LoginWrapper>
-                <LoginBox>
-                    <Input placeholder = '账号' />
-                    <Input placeholder = '密码' />
-                    <Button>登录</Button>
-                </LoginBox>
-            </LoginWrapper>
-        )
+        const { loginState } = this.props
+        if(!loginState) {
+            return (
+                <LoginWrapper>
+                    <LoginBox>
+                        <Input placeholder = '账号' innerRef = {(input) => {this.account = input}} />
+                        <Input placeholder = '密码' type = 'password' innerRef = {(input) => {this.password = input}} />
+                        <Button onClick = {() => this.props.login(this.account, this.password)}>登录</Button>
+                    </LoginBox>
+                </LoginWrapper>
+            )
+        } else {
+            return <Redirect to = '/' />
+        }
     }
 }
 
-const mapStateToProps = (state) => ({
-
+const mapState = (state) => ({
+    loginState: state.getIn(['login', 'login'])
 })
 
 const mapDispatch = (dispatch) => ({
-
+    login(accountElem, passwordElem) {
+        dispatch(actionCreators.login(accountElem.value, passwordElem.value))
+    }
 })
 
-export default connect(mapStateToProps, mapDispatch)(Login)
+export default connect(mapState, mapDispatch)(Login)
