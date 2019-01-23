9928be5641a5fc0900ef50dfa3a57f894777079e
changxianglin <281532612@qq.com>
2019-01-24 00:40:09 +0800

    write css position relative absolute and some computed

--- you/usemobx/src/pages/post/index.js
+++ you/usemobx/src/pages/post/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { observer, inject} from 'mobx-react'
+import style from './index.module.css'
 
 @inject('store')
 @observer
@@ -14,27 +15,29 @@ class Post extends Component {
                 {
                     postStore.content.map((item) => {
                         return (
-                            <div key = {item.id}>
-                                <div>
+                            <div key = {item.id} className = {style.wrapper} onClick = {() => postStore.Toggle(item.id)}>
+                                <div className = {style.left}>
                                     <div>{item.title}</div>
                                     <div>{item.desc}</div>
                                     <div>{item.content}</div>
                                 </div>
-                                <div>
-                                    <div>
-                                        <img src = {item.icon} alt = '' />
+                                <div className = {style.right}>
+                                    <div className = {style.imgSite}>
+                                        <img className = {postStore.icon[item.id] ? style.imgToggle : null} src = {item.icon} alt = '' />
                                     </div>
                                     <div>
                                         <div>{item.instructor}</div>
                                     </div>
                                 </div>    
-                                <div>
+                                <div className = {style.imgWrapper}>
                                     <img src = {item.imgUrl} alt = '' />
                                 </div>
                             </div>
                         )
                     })
                 }
+
+                <button onClick = {postStore.handleSure}>确定</button>
             </div>
         )
     }
--- you/usemobx/src/pages/post/store.js
+++ you/usemobx/src/pages/post/store.js
@@ -1,6 +1,8 @@
 import { observable, computed, action } from 'mobx'
 
 class postStore {
+    @observable flag = []
+    @observable icon = [false, false, false, false, false]
     @observable content = [
         {
             id: 1,
@@ -48,6 +50,26 @@ class postStore {
             imgUrl: '//upload.jianshu.io/admin_banners/web_images/4605/1d5cb81933dbb48ab0aa53d481a1300fc5406e7f.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540'
         }
     ]
+
+    @action Toggle = (id) => {
+        console.log('click toggle', id)
+        if(this.flag.includes(id)) {
+            console.log('includes')
+            this.flag.splice(id, 1)
+            this.icon[id] = !this.icon[id]
+        } else {
+            console.log('no includes')
+            this.icon[id] = !this.icon[id]
+            this.flag.push(id)
+        }
+    }
+
+    @action handleSure = () => {
+        console.log(this.flag.length, '选中的个数')
+        for(let i = 0; i < this.flag.length; i++) {
+            console.log(this.flag[i])
+        }
+    }
 }
 
 export default postStore
