0c13a4202df3ce30f013d05828f008e161582976
changxianglin <281532612@qq.com>
2019-01-26 01:20:51 +0800

    better components

---
+++ mooc/todo-react/src/components/AddTodo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AddTodo extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <button>Add</button>
+      </div>
+    )
+  }
+}
