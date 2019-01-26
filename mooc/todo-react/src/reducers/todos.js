796b90d462262ab662f1290a3fa7492421e8d1aa
changxianglin <281532612@qq.com>
2019-01-26 13:06:04 +0800

    create mini reducer and use combinereducer

---
+++ mooc/todo-react/src/reducers/todos.js
@@ -0,0 +1,23 @@
+import { ADD_TODO, TOGGLE_TODO } from '../action/actionTypes'
+
+const todos = (state = [], action) => {
+    switch(action.type){
+        case ADD_TODO:
+        return [
+            ...state,
+            {
+                text: action.text,
+                compoleted: false
+            }
+        ]
+        case TOGGLE_TODO:
+        return state.map(
+            todo => 
+            todo.id === action.id ? {...todo, compoleted: !todo.compoleted} : todo
+        )
+        default: 
+        return state
+    }
+}
+
+export default todos
