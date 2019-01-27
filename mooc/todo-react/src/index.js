2fdada490670f5d861441fab59e4e588da744fe5
changxianglin <281532612@qq.com>
2019-01-27 23:22:35 +0800

    react-redux

--- mooc/todo-react/src/index.js
+++ mooc/todo-react/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import store from './store'
+
 ReactDOM.render(<App />, document.getElementById('root'));
--- mooc/todo-react/src/store.js
+++ mooc/todo-react/src/store.js
@@ -6,3 +6,17 @@ const store = createStore(rootReducer)
 
 // 初始 state
 console.log(store.getState())
+
+// 订阅 state 的变化
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState())
+})
+
+// 发生 actions
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(toggleTodo(0))
+store.dispatch(setFilter('active'))
+store.dispatch(setTodoText('Learn'))
+
+// 取消订阅
+unsubscribe()
