c2d8acd4a0d9dd53c928ecb407c26524325464c7
changxianglin <281532612@qq.com>
2019-01-29 01:08:51 +0800

    create store enhancer

--- mooc/todo-react/src/index.js
+++ mooc/todo-react/src/index.js
@@ -6,13 +6,15 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App';
 
-import loggerMiddleware from './middlewares/logger'
+// import loggerMiddleware from './middlewares/logger'
+import loggerEnhancer from './enhancers/logger'
 
 const composeEnhancers =
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, 
-    composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)))
+    // composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)))
+    compose(applyMiddleware(thunkMiddleware), loggerEnhancer))
 
 ReactDOM.render(
 <Provider store = {store}>
