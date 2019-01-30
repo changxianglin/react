78999363326b9ba0e48e22583019c8b4478fd8a6
changxianglin <281532612@qq.com>
2019-01-30 23:58:39 +0800

    reselect library

--- mooc/todo-react/src/selectors/index.js
+++ mooc/todo-react/src/selectors/index.js
@@ -1,18 +1,22 @@
+import { createSelector } from 'reselect'
 export const getText = (state) => state.get('text')
 
 export const getFilter = (state) => state.get('filter')
 
-export const getVisibleTodos = (state) => {
-    const data = state.getIn(['todos', 'data'])
-    const filter = state.get('filter')
+const getTodos = state => state.todos.data
+
+export const getVisibleTodos = createSelector(
+  [getTodos, getFilter],
+  (toods, filter) => {
     switch (filter) {
       case "all":
-        return data;
+        return toods;
       case "completed":
-        return data.filter(t => t.get('completed'));
+        return toods.filter(t => t.get('completed'));
       case "active":
-        return data.filter(t => !t.get('completed'));
+        return toods.filter(t => !t.get('completed'));
       default:
         return new Error("Unkonw filter: " + filter);
     }
-  };
+  }
+)
