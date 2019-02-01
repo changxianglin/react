95658cff69c732b355e46ff3fb859a6bb5c957ea
changxianglin <281532612@qq.com>
2019-02-02 00:36:16 +0800

    headline

--- mooc/dianping-react/src/containers/Home/index.js
+++ mooc/dianping-react/src/containers/Home/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 import Category from './components/Category'
+import Headline from './components/Headline'
+
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Category />
+        <Headline />
       </div>
     )
   }
