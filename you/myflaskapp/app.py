35fac74be11f0672759b15391e8c5fccbe364ceb
changxianglin <281532612@qq.com>
2019-03-15 19:54:08 +0800

    a python programe

--- you/myflaskapp/app.py
+++ you/myflaskapp/app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id = id)
 
 if __name__ == '__main__':
     app.run(debug=True)
