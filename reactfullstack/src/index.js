a155e458774f8752984b7abd50d8babd009587ae
changxianglin <281532612@qq.com>
2019-03-20 16:07:16 +0800

    use compose appleware reduxdeltools

--- reactfullstack/src/index.js
+++ reactfullstack/src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { counter, addGUN, removeGUN, addGunAsync } from './index.redux'
 import thunk from 'redux-thunk';
 
-const store = createStore(counter, applyMiddleware(thunk))
+const store = createStore(counter, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : () => {}
+  ))
 
 function render() {
   ReactDOM.render(<App store = { store } addGUN = { addGUN } removeGUN = { removeGUN } addGunAsync = { addGunAsync } />, document.getElementById('root'))
