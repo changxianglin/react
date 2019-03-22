312088b0001580710ce739a28390129acaa369a6
changxianglin <281532612@qq.com>
2019-03-22 23:47:47 +0800

    go start

--- reactfullstack/src/index.js
+++ reactfullstack/src/index.js
@@ -7,12 +7,9 @@ import {
   Redirect,
   Switch 
  } from 'react-router-dom'
-import Auth from './Auth'
-import Dashboard from './Dashboard'
 import './config'
 
 import { createStore, applyMiddleware, compose } from 'redux'
-// import { counter } from './index.redux'
 import reducers from './reducer'
 import thunk from 'redux-thunk';
 
@@ -20,14 +17,10 @@ const store = createStore(reducers, compose(
   applyMiddleware(thunk),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   ))
+
 ReactDOM.render(
   (<Provider store = { store } >
     <BrowserRouter>
-    <Switch>
-        <Route path = '/login' exact component = {Auth}></Route>
-        <Route path = '/dashboard' component = {Dashboard}></Route>
-        <Redirect to = '/'></Redirect>
-      </Switch>
     </BrowserRouter>
   </Provider>),
 document.getElementById('root'))
--- reactfullstack/src/reducer.js
+++ reactfullstack/src/reducer.js
@@ -1,5 +1,3 @@
 import { combineReducers } from 'redux'
-import { counter } from './index.redux'
-import { auth } from './Auth.redux'
 
-export default combineReducers({counter, auth})
+export default combineReducers({})
