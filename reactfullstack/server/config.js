1ae719f3787e668ae4ec5738beb3905f226c7e9b
changxianglin <281532612@qq.com>
2019-03-24 00:50:12 +0800

    server route

--- reactfullstack/server/config.js
+++ reactfullstack/server/config.js
@@ -2,3 +2,31 @@ const mongoose = require('mongoose')
 
 const DB_URL = 'mongodb://localhost:27017/imooc'
 mongoose.connect(DB_URL)
+
+
+const models = {
+  user: {
+    'user': {type: String, require: true},
+    'pwd': {type: String, require: true},
+    'type': {type: String, require: true},
+    'avatar': {type: String},
+    'desc': {type: String},
+    'title': {type: String},
+
+    'company': {type: String},
+    'money': {type: String}
+  },
+  chat: {
+
+  }
+}
+
+for(let m in models) {
+  mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+  getModel: function(name) {
+    return mongoose.model(name)
+  }
+}
--- reactfullstack/server/user.js
+++ reactfullstack/server/user.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const Router = express.Router()
+const model = require('./config')
+const User = model.getModel('user')
 
+Router.get('/list', (req, res) => {
+  User.find({}, (err, doc) => {
+    return res.json(doc)
+  })
+})
 Router.get('/info', (req, res) => {
   return res.json({code: 1})
 })
