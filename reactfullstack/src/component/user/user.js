bb670a528e3bfed3c6f67ae2cdacec11e6a0c039
changxianglin <281532612@qq.com>
2019-03-31 17:42:30 +0800

    personal center

--- reactfullstack/src/component/user/user.js
+++ reactfullstack/src/component/user/user.js
@@ -1,21 +1,38 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Result } from 'antd-mobile'
+import { Result, List, WhiteSpace } from 'antd-mobile'
 
 @connect(
   state => state.user
 )
 class User extends React.Component {
   render() {
+    const props = this.props
+    const Item = List.Item
+    const Brief = Item.Brief
     console.log(this.props)
-    return (
+    return props.user ? (
       <div>
         <Result
-          img = {<img src = {require(`../img${this.props.avatar}.png`)} alt = '' />}
-          title = {this.props.user}
+          img = {<img src = {require(`../img/${props.avatar}.png`)} style = {{ width: 50 }} alt = '' />}
+          title = {props.user}
+          message = { props.type == 'boss' ? props.company : null }
         />
+      <List renderHeader = {() => '简介'} >
+        <Item multipleLine>
+          {props.title}
+          {props.desc.split('\n').map(v => <Brief key = {v}>{v}</Brief>)}
+          {props.money ? <Brief>薪资: {props.money}</Brief> : null}
+        </Item>
+      </List>
+      <WhiteSpace></WhiteSpace>
+      <List>
+        <Item>
+          退出登录
+        </Item>
+      </List>
       </div>
-    )
+    ) : null 
   }
 }
 
