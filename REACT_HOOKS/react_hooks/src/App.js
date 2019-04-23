01000f98e42f83985db1b6024c7d6ff0760852d0
changxianglin <281532612@qq.com>
2019-04-23 23:58:31 +0800

    useState useEffect

--- REACT_HOOKS/react_hooks/src/App.js
+++ REACT_HOOKS/react_hooks/src/App.js
@@ -1,9 +1,24 @@
- import React, { Component, useState } from 'react';
+ import React, { Component, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 export default function App() {
   const [cartItems, setCartItems] = useState([])
+  const [scrollDepath, setScrollDepath] = useState(0)
+
+  function determineUserScrollDepath() {
+    const scrolled = document.documentElement.scrollTop || document.body.scrollTop
+    setScrollDepath(scrolled)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', determineUserScrollDepath)
+    
+    return function() {
+      window.removeEventListener('scroll', determineUserScrollDepath)
+    }
+  })
+
   return (
     <div className="App">
       <h3>Hooks</h3>
@@ -15,6 +30,116 @@ export default function App() {
       >
       Add to Cart
       </button>
+      <hr />
+      <p>Yor've scrolled this far: {scrollDepath}</p>
+      <p>
+      Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+Declarative
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+Declarative views make your code more predictable and easier to debug.
+
+Component-Based
+Build encapsulated components that manage their own state, then compose them to make complex UIs.
+
+Since component logic is written in JavaScript instead of templates, you can easily pass rich data through your app and keep state out of the DOM.
+
+Learn Once, Write Anywhere
+We don’t make assumptions about the rest of your technology stack, so you can develop new features in React without rewriting existing code.
+
+React can also render on the server using Node and power mobile apps using React Native.
+
+
+      </p>
     </div>
   );
 }
