5d02a469a53c479e8ea94378ef8c38317035bbc2
changxianglin <281532612@qq.com>
2019-04-23 23:49:25 +0800

    learn react hooks

--- REACT_HOOKS/react_hooks/src/App.js
+++ REACT_HOOKS/react_hooks/src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
+ import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  const [cartItems, setCartItems] = useState([])
+  return (
+    <div className="App">
+      <h3>Hooks</h3>
+      <p>Items in your cart: {cartItems.toString()}</p>
+      <button
+        onClick = {() => {
+          setCartItems([...cartItems, 'apple'])
+        }}
+      >
+      Add to Cart
+      </button>
+    </div>
+  );
 }
-
-export default App;
