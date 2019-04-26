de9f3d6370579984964baf2096593ee05a8259ca
changxianglin <281532612@qq.com>
2019-04-26 23:41:01 +0800

    a some new function

--- REACT_HOOKS/react_hooks/src/containers/Login/components/index.js
+++ REACT_HOOKS/react_hooks/src/containers/Login/components/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import LoginHeader from './LoginHeader'
 
 class Login extends Component {
   render() {
     return (
       <div>
-
+        <LoginHeader />
       </div>
     )
   }
