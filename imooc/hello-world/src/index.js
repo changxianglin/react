80b19dccf10e1ec43a1a3b102c41cdc2fac3ebc3
changxianglin <281532612@qq.com>
2019-04-29 11:04:40 +0800

    use redux Devtools

--- imooc/hello-world/src/index.js
+++ imooc/hello-world/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducer'
 import App from './components/App.js';
 
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
 
 ReactDOM.render(
   <Provider store = {store}>
