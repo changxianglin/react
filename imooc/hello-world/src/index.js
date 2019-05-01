2a564d0dd7be151754feb2169b7b6db2cb8ae413
changxianglin <281532612@qq.com>
2019-05-01 23:56:41 +0800

    middleware

--- imooc/hello-world/src/index.js
+++ imooc/hello-world/src/index.js
@@ -5,9 +5,10 @@ import { Provider } from 'react-redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from './reducer'
 import App from './components/App.js';
+import loggerMiddleware from './middlewares/logger'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)))
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware, loggerMiddleware)))
 
 ReactDOM.render(
   <Provider store = {store}>
