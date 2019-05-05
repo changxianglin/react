30e8d58ca7782cc30aed8bc69edc3760e29a3dab
changxianglin <281532612@qq.com>
2019-05-05 23:59:21 +0800

    more route

--- imooc/react-touer-samples/src/User.js
+++ imooc/react-touer-samples/src/User.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import {Route} from 'react-router-dom'
 
 export default class User extends Component {
   render() {
     const { match } = this.props
     return (
       <div>
+        <Route path = {`${match.path}/:user`} component = {userDetail} />
         User: { match.params.user }
       </div>
     )
