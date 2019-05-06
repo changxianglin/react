b71d1855120fd09b658cee547d7a74c27cc18e35
changxianglin <281532612@qq.com>
2019-05-06 22:43:21 +0800

    write input

---
+++ imooc/todolist/src/TodoList.js
@@ -0,0 +1,18 @@
+import React, { Fragment} from 'react';
+
+function TodoList() {
+  return (
+    <Fragment>
+      <div>
+        <input />
+        <button>提交</button>
+      </div>
+        <ul>
+          <li>学音乐</li>
+          <li>learing React</li>
+        </ul>
+    </Fragment>
+  );
+}
+
+export default TodoList;
