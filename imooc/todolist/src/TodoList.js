888707d5a6d5c8f6ce5d079b8d70066188b389fd
changxianglin <281532612@qq.com>
2019-05-06 22:58:00 +0800

    setstate

--- imooc/todolist/src/TodoList.js
+++ imooc/todolist/src/TodoList.js
@@ -1,18 +1,37 @@
 import React, { Fragment} from 'react';
+import { deflateRaw } from 'zlib';
 
-function TodoList() {
-  return (
-    <Fragment>
-      <div>
-        <input />
-        <button>提交</button>
-      </div>
-        <ul>
-          <li>学音乐</li>
-          <li>learing React</li>
-        </ul>
-    </Fragment>
-  );
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputValue: '',
+      list: [],
+    }
+    this.handleInputChnage = this.handleInputChnage.bind(this)
+  }
+
+  render(){
+    return (
+      <Fragment>
+        <div>
+          <input value = {this.state.inputValue} onChange = {this.handleInputChnage} />
+          <button>提交</button>
+        </div>
+          <ul>
+            <li>学音乐</li>
+            <li>learing React</li>
+          </ul>
+      </Fragment>
+    );
+  }
+
+  handleInputChnage(e) {
+    this.setState({
+      inputValue: e.target.inputValue
+    })
+    
+  }
 }
 
-export default TodoList;
+export default TodoList
