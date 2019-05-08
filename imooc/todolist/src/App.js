33d4bb70e40b40d12647471c27a245c3ffe0d273
changxianglin <281532612@qq.com>
2019-05-09 02:34:24 +0800

    react-transition-group transitiongroup

--- imooc/todolist/src/App.js
+++ imooc/todolist/src/App.js
@@ -1,37 +1,43 @@
 import React, { Component, Fragment } from 'react'
-import { CSSTransition } from 'react-transition-group'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import './style.css'
 
 export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      show: true
+      list: []
     }
   }
 
   render() {
     return (
       <Fragment>
-        <CSSTransition
-          in = {this.state.show}
+        <TransitionGroup>
+        {
+          this.state.list.map((item, index) => {
+            return (
+              <CSSTransition
           timeout = { 1000 }
           classNames = 'fade'
           onEnter = { (el) => {el.style.color = 'blue'}}
           appear = { true }
+          key = { index }
         >
-        <div>
-        Hello
-      </div>
-      </CSSTransition>
-      <button onClick = { this.handleToggle }>Toggle</button>
+              <div>{ item }</div>
+          </CSSTransition>
+            )
+          })
+        }
+        </TransitionGroup>
+      <button onClick = { this.handleAddItem }>Toggle</button>
       </Fragment>
     )
   }
 
-  handleToggle = () => {
-    this.setState(() => ({
-      show: !this.state.show
+  handleAddItem = () => {
+    this.setState((prevState) => ({
+      list: [...prevState.list, 'item']
     }))
   }
 }
