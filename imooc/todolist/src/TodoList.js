9beb4bd15dc0da83d3f2abf15cd473c8fb0a553a
changxianglin <281532612@qq.com>
2019-05-09 10:28:17 +0800

    todolist use antd

---
+++ imooc/todolist/src/TodoList.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd'
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div style = {{ marginTop: '10px', marginLeft: '10px' }}>
+        <div>
+          <Input placeholder = 'todo info' style = {{ width: "300px", marginRight: '10px' }} />
+          <Button type = "primary">提交</Button>
+          <List
+            bordered
+            style = {{ marginTop: '10px', width: '300px' }}
+            dataSource = { data }
+            renderItem = { item => (<List.Item>{ item }</List.Item>)}
+          />
+        </div> 
+      </div>
+    )
+  }
+}
+
+export default TodoList
