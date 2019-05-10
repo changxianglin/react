6e09978646c0cb1e60405daed7f36d92a44e8f80
changxianglin <281532612@qq.com>
2019-05-10 09:22:51 +0800

    redux store reducer action dispatch

--- imooc/todolist/src/TodoList.js
+++ imooc/todolist/src/TodoList.js
@@ -7,15 +7,25 @@ class TodoList extends Component {
   constructor(props) {
     super(props)
     this.state = store.getState()
-    console.log(store.getState())
+    
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleStoreChange = this.handleStoreChange.bind(this)
+    this.handleBtnClick = this.handleBtnClick.bind(this)
+
+    store.subscribe(this.handleStoreChange)
   }
 
   render() {
     return (
       <div style = {{ marginTop: '10px', marginLeft: '10px' }}>
         <div>
-          <Input value = {this.state.inputValue} placeholder = 'todo info' style = {{ width: "300px", marginRight: '10px' }} />
-          <Button type = "primary">提交</Button>
+          <Input 
+            value = {this.state.inputValue} 
+            placeholder = 'todo info' 
+            style = {{ width: "300px", marginRight: '10px' }} 
+            onChange = { this.handleInputChange }
+          />
+          <Button type = "primary" onClick = { this.handleBtnClick }>提交</Button>
           <List
             bordered
             style = {{ marginTop: '10px', width: '300px' }}
@@ -25,6 +35,25 @@ class TodoList extends Component {
         </div> 
       </div>
     )
+  }
+
+  handleInputChange(e) {
+    const action = {
+      type: 'change_input_value',
+      value: e.target.value,
+    }
+    store.dispatch(action)
+  }
+
+  handleStoreChange() {
+    this.setState(store.getState())
+  }
+
+  handleBtnClick() {
+    const action = {
+      type: 'add_todo_item',
+    }
+    store.dispatch(action)
   }
 }
 
--- imooc/todolist/src/store/reducer.js
+++ imooc/todolist/src/store/reducer.js
@@ -4,5 +4,16 @@ const defaultState = {
 }
 
 export default (state = defaultState, action) => {
+  if(action.type === 'change_input_value') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value
+    return newState
+  } else if(action.type = 'add_todo_item') {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+  console.log(state, action)
   return state
 }
