a4d79e8fbcd2214671a93c57e9ad040ec60a0d38
changxianglin <281532612@qq.com>
2019-05-11 15:04:02 +0800

    none state component

--- imooc/todolist/src/TodoListUI.js
+++ imooc/todolist/src/TodoListUI.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 import { Input, Button, List } from 'antd'
 
-export default class TodoListUI extends Component {
-  render() {
-    return (
-      <div style = {{ marginTop: '10px', marginLeft: '10px' }}>
+const TodoListUI = (props) => {
+  return (
+    <div style = {{ marginTop: '10px', marginLeft: '10px' }}>
         <div>
           <Input 
-            value = {this.props.inputValue} 
+            value = {props.inputValue} 
             placeholder = 'todo info' 
             style = {{ width: "300px", marginRight: '10px' }} 
-            onChange = { this.props.handleInputChange }
+            onChange = { props.handleInputChange }
           />
-          <Button type = "primary" onClick = { this.props.handleBtnClick }>提交</Button>
+          <Button type = "primary" onClick = { props.handleBtnClick }>提交</Button>
           <List
             bordered
             style = {{ marginTop: '10px', width: '300px' }}
-            dataSource = { this.props.list }
-            renderItem = { (item, index) => (<List.Item onClick = {(index) => {this.props.handleItemClick(index)}}>{ item }</List.Item>)}
+            dataSource = { props.list }
+            renderItem = { (item, index) => (<List.Item onClick = {(index) => {props.handleItemClick(index)}}>{ item }</List.Item>)}
           />
         </div> 
       </div>
-    )
-  }
+  )
 }
+
+export default TodoListUI
