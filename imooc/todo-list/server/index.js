9677970a7f2d5829922c7ebdf825d5b0cb3de425
changxianglin <281532612@qq.com>
2019-05-16 23:02:06 +0800

    server and client need to proxy use setupProxy

---
+++ imooc/todo-list/server/index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+
+const port = 5000
+const start = () => {
+  console.log('app running 5000 port')
+}
+
+let data = {
+  "name": 'zhangsan',
+  "age": 27,
+  "gender": "male",
+  "timer": Math.random().toFixed(2) * 100
+}
+
+let data1 = {
+  "name": 'lisi',
+  "age": 23,
+  "gender": 'female',
+  "timer": Math.random().toFixed(1) * 10
+}
+
+app.get('/api/test/:testId', (req, res) => {
+  console.log(req.params)
+  res.send(data)
+})
+
+app.get('/api/test', (req, res) => {
+  res.send(data)
+})
+
+app.post('/api/foo', (req, res) => {
+  res.send(data1)
+})
+
+
+app.listen(port, start())
--- imooc/todo-list/src/components/AddTodo.js
+++ imooc/todo-list/src/components/AddTodo.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import qs from 'qs'
 
 export default class AddTodo extends Component {
   render() {
@@ -7,6 +9,7 @@ export default class AddTodo extends Component {
       <div>
         <input value = { text } onChange = { this.handleChange } />
         <button onClick = { this.handleClick }>Add</button>
+        <button onClick = { this.handleTest }>ADD TEST</button>
       </div>
     )
   }
@@ -17,7 +20,30 @@ export default class AddTodo extends Component {
   }
 
   handleClick = () => {
+    let queryString = Math.random().toFixed(3) * 1000
+    axios.get('/api/test/', {
+      params: {
+        testId: queryString
+      }
+    }).then(res => {
+      console.log(res.data)
+    })
     const { addTodo, text } = this.props
     addTodo(text)
+  }
+
+  handleTest = () => {
+    const sendData = {
+      'name': 'wanger',
+      'age': 25,
+      'gender': "male",
+    }
+    axios.post('/api/foo', {
+      params: {
+        fooId: qs.stringify(sendData)
+      }
+    }).then(res => {
+      console.log(res.data)
+    })
   }
 }
