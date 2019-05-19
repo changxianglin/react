d9adbe506aaa03461ce38ee54f0b81aa97134ef8
changxianglin <281532612@qq.com>
2019-05-19 23:53:00 +0800

    inital state

---
+++ imooc/dianping-react/src/redux/modules/entities/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from 'redux'
+import products from './products'
+import shops from './shops'
+import orders from './orders'
+import comments from './comments'
+
+const rootReducer = combineReducers({
+  products,
+  shops,
+  orders,
+  comments,
+})
+
+export default rootReducer
