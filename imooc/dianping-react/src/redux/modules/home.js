89e813a6b9bbe185a7c8298886901e10220eefa7
changxianglin <281532612@qq.com>
2019-05-20 00:31:01 +0800

    start request data

--- imooc/dianping-react/src/redux/modules/home.js
+++ imooc/dianping-react/src/redux/modules/home.js
@@ -1,5 +1,50 @@
+import { get } from '../../utils/request'
+import url from '../../utils/url'
+
+export const types = {
+  FETCH_LIKES_REQUEST: "HOME/FETCH_LIKES_REQUEST", //获取猜你喜欢请求
+  FETCH_LIKES_SUCCESS: "HOME/FETCH_LIKES_SUCCESS", //获取猜你喜欢请求成功
+  FETCH_LIKES_FAILURE: "HOME/FETCH_LIKES_FAILURE", //获取猜你喜欢请求失败
+}
+
+export const actions = {
+  loadLikes: () => {
+    return (dispatch, getState) => {
+      dispatch(fetchLikesRequest())
+      return get(url.getProductList(0, 10)).then(
+        data => {
+          dispatch(fetchLikesSuccess(data))
+        },
+        error => {
+          dispatch(fetchLikesFailure(error))
+        }
+      )
+    }
+  }
+}
+
+const fetchLikesRequest = () => ({
+  type: types.FETCH_LIKES_REQUEST,
+})
+
+const fetchLikesSuccess = (data) => ({
+  type: types.FETCH_LIKES_SUCCESS,
+  data,
+})
+
+const fetchLikesFailure = (error) => ({
+  type: types.FETCH_LIKES_FAILURE,
+  error,
+})
+
 const reducer = (state = {}, action) => {
-  return state 
+  switch(action.type) {
+    case types.FETCH_LIKES_REQUEST:
+    case types.FETCH_LIKES_SUCCESS:
+    case types.FETCH_LIKES_FAILURE:
+    default:
+      return state
+  }
 }
 
 export default reducer
