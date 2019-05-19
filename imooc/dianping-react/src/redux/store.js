9d33a968d4915acdc9d4b58ded71ee7c3e04bcec
changxianglin <281532612@qq.com>
2019-05-19 23:59:23 +0800

    create store

--- imooc/dianping-react/src/redux/store.js
+++ imooc/dianping-react/src/redux/store.js
@@ -2,8 +2,16 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk' 
 import rootReducer from './modules'
 
-let store = createStore(rootReducer, applyMiddleware(
-  thunk,
-))
+let store
+if( process.env.NODE_ENV !== "production" && window.__REDUX_DEVTOOLS_EXTENSION__) {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ))
+} else {
+  store = createStore(rootReducer, applyMiddleware(
+    thunk,
+  ))
+}
 
 export default store
