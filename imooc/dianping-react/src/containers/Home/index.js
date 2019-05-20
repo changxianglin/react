8405ebce62da02aed5f014800cc259d8433c6fe0
changxianglin <281532612@qq.com>
2019-05-20 16:20:52 +0800

    create home headline component

--- imooc/dianping-react/src/containers/Home/index.js
+++ imooc/dianping-react/src/containers/Home/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Category from './components/Category'
+import HeadLine from './components/Headline'
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Category />
+        <HeadLine />
       </div>
     )
   }
