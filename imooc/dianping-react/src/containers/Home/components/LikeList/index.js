6521f4a116e70bab599b511584de5f966f89b472
changxianglin <281532612@qq.com>
2019-05-21 23:09:11 +0800

    loading more data

--- imooc/dianping-react/src/containers/Home/components/LikeList/index.js
+++ imooc/dianping-react/src/containers/Home/components/LikeList/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import LikeItem from '../LikeItem'
+import Loading from '../../../../components/Loading'
 import './style.css'
 
 const dataSource = [
@@ -66,19 +67,73 @@ const dataSource = [
 ];
 
 export default class LikeList extends Component {
+  constructor(props) {
+    super(props)
+    this.myRef = React.createRef();
+    this.state = {
+      data: dataSource,
+      loadTimes: 1,
+    }
+    this.removeListener = false
+  }
+
   render() {
-    const data = dataSource
+    const {data, loadTimes} = this.state
     return (
-      <div className = 'likeList'> 
+      <div ref = {this.myRef} className = 'likeList'> 
         <div className = 'likeList__header'>猜你喜欢</div>
         <div className = 'likeList__list'>
           {
             data.map((item, index) => {
-              return <LikeItem key = {item.id} data = {item} />
+              return <LikeItem key = {index} data = {item} />
             })
           }
         </div>
+        {
+          loadTimes < 3 ? (
+            <Loading />
+          ) : (
+            <a className = 'likeList__viewAll'>
+              查看更多
+            </a>
+          )
+        }
       </div>
     )
+  }
+
+  componentDidMount() {
+    document.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentDidUpdate() {
+    if(this.state.loadTimes >= 3 && !this.removeListener) {
+      document.removeEventListener('scroll', this.handleScroll)
+      this.removeListener = true
+    }
+  }
+
+  componentWillUnmount() {
+    if(!this.removeListener) {
+      document.removeEventListener('scroll', this.handleScroll)
+    }
+  }
+
+  // 处理屏幕滚动事件, 实现更多的效果
+  handleScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop 
+    const screenHeight = document.documentElement.clientHeight;
+    const likeListTop = this.myRef.current.offsetTop;
+    const likeListHeight = this.myRef.current.offsetHeight;
+    if(scrollTop >= likeListHeight + likeListTop - screenHeight) {
+      const newData = this.state.data.concat(dataSource)
+      const newLoadTimes = this.state.loadTimes + 1
+      setTimeout(() => {
+        this.setState({
+          data: newData,
+          loadTimes: newLoadTimes,
+        })
+      }, 1000)
+    }
   }
 }
