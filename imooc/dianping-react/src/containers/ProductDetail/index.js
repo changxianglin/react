3e8c7f0b210a1fb375a1a576d1046914631052e1
changxianglin <281532612@qq.com>
2019-05-25 23:42:11 +0800

    create shopinfo component

--- imooc/dianping-react/src/containers/ProductDetail/index.js
+++ imooc/dianping-react/src/containers/ProductDetail/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import ProductOverview from './components/ProductOverview';
+import ShopInfo from './components/ShopInfo'
 
 export default class ProductDetail extends Component {
   render() {
     return (
       <div>
-        
+        <ProductOverview />
+        <ShopInfo />
       </div>
     )
   }
