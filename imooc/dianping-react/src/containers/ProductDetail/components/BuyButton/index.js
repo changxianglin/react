dfe4eedd4f842a0cc9bb500e90850c273f803dc3
changxianglin <281532612@qq.com>
2019-05-27 16:00:19 +0800

    create detail other components

---
+++ imooc/dianping-react/src/containers/ProductDetail/components/BuyButton/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './style.css'
+
+export default class BuyButton extends Component {
+  render() {
+    return (
+      <a className = 'buyButton'>
+        立即购买
+      </a>
+    )
+  }
+}
--- imooc/dianping-react/src/containers/ProductDetail/index.js
+++ imooc/dianping-react/src/containers/ProductDetail/index.js
@@ -3,16 +3,24 @@ import ProductOverview from './components/ProductOverview';
 import ShopInfo from './components/ShopInfo'
 import Detail from './components/Detail';
 import Remark from './components/Remark';
+import BuyButton from './components/BuyButton';
+import Header from '../../components/Header';
 
 export default class ProductDetail extends Component {
   render() {
     return (
       <div>
+        <Header title = '团购详情' onBack = {this.handleBack} grey  /> 
         <ProductOverview />
         <ShopInfo />
         <Detail />
         <Remark />
+        <BuyButton />
       </div>
     )
+  }
+
+ handleBack = () => {
+
   }
 }
