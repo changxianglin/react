41080aa01fd1f1e9b263c583f80fbb17806385c9
changxianglin <281532612@qq.com>
2019-05-31 03:06:48 +0800

    create childen reducer

--- imooc/dianping-react/src/redux/modules/search.js
+++ imooc/dianping-react/src/redux/modules/search.js
@@ -1,7 +1,26 @@
+import { combineReducers } from 'redux'
+import url from '../../utils/url'
+import { FETCH_DATA } from '../middleware/api'
+import { schema as keywordSchema } from './entities/keywords'
+
 export const types = {
+  // 获取热门关键词
   FETCH_POPULAR_KEYWORDS_REQUEST: 'SEARCH/FETCH_POPULAR_KEYWORDS_REQUEST',
   FETCH_POPULAR_KEYWORDS_SUCCESS: 'SEARCH/FETCH_POPULAR_KEYWORDS_SUCCESS',
   FETCH_POPULAR_KEYWORDS_FAILURE: 'SEARCH/FETCH_POPULAR_KEYWORDS_FAILURE',
+
+  // 根据输入的文本获取相关的关键词
+  FETCH_RELATED_KEYWORDS_REQUEST: 'SEARCH/FETCH_RELATED_KEYWORDS_REQUEST',
+  FETCH_RELATED_KEYWORDS_SUCCESS: 'SEARCH/FETCH_RELATED_KEYWORDS_SUCCESS',
+  FETCH_RELATED_KEYWORDS_FAILURE: 'SEARCH/FETCH_RELATED_KEYWORDS_FAILURE',
+
+  // 设置当前输入
+  SET_INPUT_TEXT: 'SEARCH/SET_INPUT_TEXT',
+  CLEAR_INPUT_TEXT: 'SEARCH/CLEAR_INPUT_TEXT',
+
+  // 历史查询记录
+  ADD_HISTORY_KEYWORD: 'SEARCH/ADD_HISTORY_KEYWORD',
+  CLEAR_HISTORY_KEYWORDS: 'SEARCH/CLEAR_HISTORY_KEYWORDS'
 } 
 
 const initialState = {
@@ -15,3 +34,154 @@ const initialState = {
   },
   historyKeywords: []
 }
+
+export const action = {
+  // 获取热门关键词
+  loadPopularKeywords: () => {
+    return (dispatch, getState) => {
+      const { id } = getState().search.popularKeywords
+      if(ids.length > 0) {
+        return null
+      }
+      const endpoint = url.getPopularKeywords()
+      return dispatch(fetchPopularKeywords(endpoint))
+    }
+  },
+  // 根据输入获取相关关键词
+  loadRelatedKeywords: (text) => {
+    return (dispatch, getState) => {
+      const { relatedKeywords } = getState().search
+      if(relatedKeywords[text]) {
+        return null
+      }
+      const endpoint = url.getRelatedKeywords(text)
+      return dispatch(fetchRelatedKeywords(text, endpoint))
+    }
+  },
+  setInputText: text => ({
+    type: types.SET_INPUT_TEXT,
+    text,
+  }),
+  clearInputText: () => ({
+    type: types.CLEAR_INPUT_TEXT,
+  }),
+  addHistoryKeyword: keywordId => ({
+    type: types.ADD_HISTORY_KEYWORD,
+    text: keywordId,
+  }),
+  clearHistoryKeywords: () => ({
+    type: types.CLEAR_HISTORY_KEYWORDS
+  })
+}
+
+const fetchPopularKeywords = (endpoint) => ({
+  [FETCH_DATA]: {
+    types: [
+      types.FETCH_POPULAR_KEYWORDS_REQUEST,
+      types.FETCH_POPULAR_KEYWORDS_SUCCESS,
+      types.FETCH_POPULAR_KEYWORDS_FAILURE,
+    ],
+    endpoint,
+    schema: keywordSchema,
+  }
+})
+
+const fetchRelatedKeywords = (text, endpoint) => ({
+  [FETCH_DATA]: {
+    types: [
+      types.FETCH_RELATED_KEYWORDS_REQUEST,
+      types.FETCH_RELATED_KEYWORDS_SUCCESS,
+      types.FETCH_RELATED_KEYWORDS_FAILURE,
+    ],
+    endpoint,
+    schema: keywordSchema,
+  },
+  text,
+})
+
+const popularkeywords = (state = initialState.popularKeywords, action) => {
+  switch(action.type) {
+    case types.FETCH_POPULAR_KEYWORDS_REQUEST:
+      return {...state, isFetching: true}
+    case types.FETCH_POPULAR_KEYWORDS_SUCCESS:
+      return {
+        ...state, 
+        isFetching: false,
+        ids: state.ids.concat(action.response.ids) 
+      }
+    case types.FETCH_POPULAR_KEYWORDS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+      }
+    default:
+      return state
+  }
+}
+
+const relatedKeywords = (state = initialState.relatedKeywords, action) => {
+  switch(action.type) {
+    case types.FETCH_RELATED_KEYWORDS_REQUEST:
+    case types.FETCH_RELATED_KEYWORDS_SUCCESS:
+    case types.FETCH_RELATED_KEYWORDS_FAILURE:
+      return {
+        ...state,
+        [action.text]: relatedKeywordsByText(state[action.text], action),
+      }
+    default:
+      return state
+  }
+}
+
+const relatedKeywordsByText = (state = {isFetching: false, ids: []}, action) => {
+  switch(action.type) {
+    case types.FETCH_RELATED_KEYWORDS_REQUEST:
+      return {...state, isFetching: true}
+    case types.FETCH_RELATED_KEYWORDS_SUCCESS:
+      return {
+        ...state, 
+        isFetching: false, 
+        ids: state.ids.concat(action.response.ids),
+      }
+    case types.FETCH_RELATED_KEYWORDS_FAILURE:
+      return {...state, isFetching: false}
+    default:
+      return state
+  }
+}
+
+const inputText = (state = initialState.inputText, action) => {
+  switch(action.type) {
+    case types.SET_INPUT_TEXT:
+      return action.text
+    case types.CLEAR_INPUT_TEXT:
+      return ''
+    default:
+      return state
+  }
+}
+
+const historyKeywords = (state = initialState.historyKeywords, action) => {
+  switch(action.type) {
+    case types.ADD_HISTORY_KEYWORD:
+      const data = state.filter(item => {
+        if(item != action.text) {
+          return true
+        }
+      })
+      return [action.text, ...data]
+    case types.CLEAR_HISTORY_KEYWORDS:
+      return []
+    default:
+      return state
+  }
+}
+
+const reducer = combineReducersl({
+  popularKeywords,
+  relatedKeywords,
+  inputText,
+  historyKeywords,
+})
+
+export default reducer
--- imooc/dianping-react/src/utils/url.js
+++ imooc/dianping-react/src/utils/url.js
@@ -2,4 +2,6 @@ export default {
   getProductList: (path, rowIndex, pageSize) =>  `/mock/products/${path}.json?rowIndex=${rowIndex}&pageSize=${pageSize}`,
   getProductDetail: (id) => `/mock/product_detail/${id}.json`,
   getShopById: (id) => `/mock/shops/${id}.json`,
+  getPopularKeywords: () => `/mock/keywords/popular.json`,
+  getRelatedKeywords: (text) => `/mock/keywords/related.json?keyword=${text}`
 }
