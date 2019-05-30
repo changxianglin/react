78b44b02b99433c542be726abc0d8a6e192de62c
changxianglin <281532612@qq.com>
2019-05-30 23:18:49 +0800

    crate popularSearch

---
+++ imooc/dianping-react/src/containers/Search/components/PopularSearch/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import './style.css'
+
+const data = ['三里屯','朝阳大悦城','西单','海底捞','星巴克','局气','火锅','温泉','烤鸭']
+
+export default class PopularSearch extends Component {
+  render() {
+    return (
+      <div className = 'popularSearch'>
+        {
+          data.map((item, index) => {
+            return (
+              <span key = {index} className = 'popularSearch__item'>{item}</span>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
--- imooc/dianping-react/src/containers/Search/index.js
+++ imooc/dianping-react/src/containers/Search/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import SearchBox from './components/SearchBox';
+import PopularSearch from './components/PopularSearch'
 
 export default class Search extends Component {
   render() {
     return (
       <div>
         <SearchBox />
+        <PopularSearch />
       </div>
     )
   }
