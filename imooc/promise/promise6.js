90d664e7c2b87f01f6562710f7df215d15cac3ab
changxianglin <281532612@qq.com>
2019-06-17 23:49:10 +0800

    promise inner promise can use then

---
+++ imooc/promise/promise6.js
@@ -0,0 +1,28 @@
+console.log('Start')
+
+new Promise(resolve => {
+  console.log('Step 1')
+  setTimeout(() => {
+    resolve(100)
+  }, 1000)
+})
+  .then(value => {
+    return new Promise(resolve => {
+      console.log('Step 1-1')
+      setTimeout(() => {
+        resolve(110)
+      }, 1000)
+    })
+  })
+  .then(value => {
+    console.log('Step 1-2')
+    return value
+  })
+  .then(value => {
+    console.log('Step 1-3')
+    return value
+  })
+  .then(value => {
+    console.log(value)
+    console.log('Step 2')
+  })
