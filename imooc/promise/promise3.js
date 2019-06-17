bef95eeda2d011b2a7fa654dd17d50fcd93c9be4
changxianglin <281532612@qq.com>
2019-06-17 23:20:22 +0800

    more one Promise situation

---
+++ imooc/promise/promise3.js
@@ -0,0 +1,18 @@
+console.log('here we go')
+
+new Promise(resolve => {
+  setTimeout(() => {
+    resolve('hello')
+  }, 2000)
+})
+  .then(value => {
+    console.log(value)
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('world')
+      }, 2000)
+    })
+  })
+  .then(value => {
+    console.log(value + ' world')
+  })
