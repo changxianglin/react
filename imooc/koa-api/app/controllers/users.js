9e7a3e3b402ac7cdbad1b5b745f8a67124ab7c80
changxianglin <281532612@qq.com>
2019-06-18 20:14:44 +0800

    koa error middleware

--- imooc/koa-api/app/controllers/users.js
+++ imooc/koa-api/app/controllers/users.js
@@ -2,9 +2,13 @@ let db = [{name: '李四'}]
 
 class UsersCtl {
   find(ctx) {
+    a.b
     ctx.body = db
   }
   findById(ctx) {
+    if(ctx.params.id * 1 >= db.length) {
+      ctx.throw(412, '你发送了错误的 id ')
+    }
     ctx.body = db[ctx.params.id * 1]
   }
   create(ctx) {
--- imooc/koa-api/app/index.js
+++ imooc/koa-api/app/index.js
@@ -3,8 +3,17 @@ const bodyparser = require('koa-bodyparser')
 const app = new Koa()
 const routing = require('./routes')
 
+app.use(async (ctx, next) => {
+  try {
+    await next()
+  } catch(err) {
+    ctx.status = err.status || err.statusCode || 500
+    ctx.body = {
+      message: err.message
+    }
+  }
+})
 app.use(bodyparser())
 routing(app)
-
 
 app.listen(3000, () => console.log('app running'))
