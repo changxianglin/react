c29c042e3c744e16a96caa75e35a1c21be396c8a
changxianglin <281532612@qq.com>
2019-06-19 08:17:51 +0800

    arguments parameter

--- imooc/koa-api/app/controllers/users.js
+++ imooc/koa-api/app/controllers/users.js
@@ -12,14 +12,28 @@ class UsersCtl {
     ctx.body = db[ctx.params.id * 1]
   }
   create(ctx) {
+    ctx.verifyParams({
+      name: { type: 'string', required: true },
+      age: { type: 'number', required: false }
+    })
     db.push(ctx.request.body)
     ctx.body = ctx.request.body
   }
   update(ctx) {
+    if(ctx.params.id * 1 >= db.length) {
+      ctx.throw(412, '你发送了错误的 id ')
+    }
+    ctx.verifyParams({
+      name: { type: 'string', required: true },
+      age: { type: 'number', required: false }
+    })
     db[ctx.params.id * 1] = ctx.request.body 
     ctx.body = ctx.request.body
   }
   delete(ctx) {
+    if(ctx.params.id * 1 >= db.length) {
+      ctx.throw(412, '你发送了错误的 id ')
+    }
     db.splice(ctx.params.id * 1, 1)
     ctx.status = 204
   }
--- imooc/koa-api/app/index.js
+++ imooc/koa-api/app/index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
+const parameter = require('koa-parameter')
 const app = new Koa()
 const routing = require('./routes')
 
@@ -8,6 +9,7 @@ app.use(error({
   postFormat: (e, { stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest}
 }))
 app.use(bodyparser())
+app.use(parameter(app))
 routing(app)
 
 app.listen(3000, () => console.log('app running'))
