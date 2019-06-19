fdf0bf2ffc8f5aa07f04a9a88b79b617adb9d0b5
changxianglin <281532612@qq.com>
2019-06-19 08:09:44 +0800

    use koa-json-error

--- imooc/koa-api/app/index.js
+++ imooc/koa-api/app/index.js
@@ -1,18 +1,12 @@
 const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
+const error = require('koa-json-error')
 const app = new Koa()
 const routing = require('./routes')
 
-app.use(async (ctx, next) => {
-  try {
-    await next()
-  } catch(err) {
-    ctx.status = err.status || err.statusCode || 500
-    ctx.body = {
-      message: err.message
-    }
-  }
-})
+app.use(error({
+  postFormat: (e, { stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest}
+}))
 app.use(bodyparser())
 routing(app)
 
