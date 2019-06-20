15c4d3e892f1273e88ce0da398f1332dcaae3777
changxianglin <281532612@qq.com>
2019-06-20 23:53:37 +0800

    use MongoDB

--- imooc/koa-api/app/index.js
+++ imooc/koa-api/app/index.js
@@ -2,9 +2,13 @@ const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
+const mongoose = require('mongoose')
 const app = new Koa()
 const routing = require('./routes')
+const { connectionStr } = require('./config')
 
+mongoose.connect(connectionStr, { useNewUrlParser: true }, () => console.log('MongoDB link success...'))
+mongoose.connection.on('error', console.error)
 app.use(error({
   postFormat: (e, { stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest : { stack, ...rest}
 }))
