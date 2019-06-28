3fa8d25a7b4b9e0294618183441844968b3ac377
changxianglin <281532612@qq.com>
2019-06-28 23:51:01 +0800

    header more itme

--- imooc/jianshu/src/common/header/index.js
+++ imooc/jianshu/src/common/header/index.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react'
 import {
     HeaderWrapper,
     Logo,
-    Nav
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button,
 } from './style'
-export default class header extends Component {
+export default class Header extends Component {
   render() {
     return (
       <div>
         <HeaderWrapper>
           <Logo href = '/' />
           <Nav>
-            
+            <NavItem className = 'left active'>首页</NavItem>
+            <NavItem className = 'left'>下载App</NavItem>
+            <NavItem className = 'right'>登录</NavItem>
+            <NavItem className = 'right'>Aa</NavItem>
+            <NavSearch></NavSearch>
           </Nav>
+          <Addition>
+            <Button className = 'writting'>写文章</Button>
+            <Button className = 'reg'>注册</Button>
+          </Addition>
         </HeaderWrapper>
       </div>
     )
