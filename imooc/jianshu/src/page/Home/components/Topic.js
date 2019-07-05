b379ddd3673ccb0373791b3110afc4a4c8154931
changxianglin <281532612@qq.com>
2019-07-05 23:59:31 +0800

    create home reducer

--- imooc/jianshu/src/page/Home/components/Topic.js
+++ imooc/jianshu/src/page/Home/components/Topic.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import {TopicWrapper, TopicItem } from '../style'
 
 export default class Topic extends Component {
   render() {
     return (
-      <div>
-        Topic
-      </div>
+      <TopicWrapper>
+        <TopicItem>
+          <img
+           className = 'topic-pic'
+           src = "//upload.jianshu.io/collections/images/261938/man-hands-reading-boy-large.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64" alt = ''
+           />
+          热会热点
+        </TopicItem>
+      </TopicWrapper>
     )
   }
 }
---
+++ imooc/jianshu/src/page/Home/store/reducer.js
@@ -0,0 +1,12 @@
+import { fromJS } from 'immutable'
+
+const initialState = {
+
+}
+
+export default (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
