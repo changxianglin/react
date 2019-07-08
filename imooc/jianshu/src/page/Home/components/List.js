b6fed596dca2dc6dda497f8cd1cc9696cd337617
changxianglin <281532612@qq.com>
2019-07-08 23:58:49 +0800

    write list components

--- imooc/jianshu/src/page/Home/components/List.js
+++ imooc/jianshu/src/page/Home/components/List.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { ListItem, ListInfo } from '../style'
 
 export default class List extends Component {
   render() {
     return (
-      <div>
-        List
-      </div>
+      <ListItem>
+        <img className = 'pic' src = '//upload-images.jianshu.io/upload_images/17128869-1f4a92053c97e33c.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/360/h/240' alt = '' />
+        <ListInfo>
+          <h3 className = 'title'>鬼知道这些程序员在网吧写代码时都经历了什么</h3>
+          <p className = 'desc'>
+      在网吧写代码是怎样一种体验？ @Vivu 装完开发环境，重启，没了 @落在起风的地方 写了个哈喽沃德 哎 打刀塔吧 @Debby 去网吧花两个小...
+    </p>
+          </ListInfo>
+      </ListItem>
     )
   }
 }
--- imooc/jianshu/src/page/Home/style.js
+++ imooc/jianshu/src/page/Home/style.js
@@ -26,6 +26,7 @@ export const TopicWrapper = styled.div`
   overflow: hidden;
   padding: 20px 0 10px 0;
   margin-left: -18px;
+  border-bottom: 1px solid #dcdcdc;
 `
 
 export const TopicItem = styled.div`
@@ -46,5 +47,34 @@ export const TopicItem = styled.div`
     width: 32px;
     height: 32px;
     margin-right: 10px;
+  }
+`
+
+export const ListItem = styled.div`
+  overflow: hidden;
+  padding: 20px 0;
+  border-bottom: 1px solid #dcdcdc;
+  .pic {
+    display: block;
+    width: 125px;
+    height: 100px;
+    float: right;
+    border-radius: 10px;
+  }
+`
+
+export const ListInfo = styled.div`
+  width: 500px;
+  float: left;
+  .title {
+    line-height: 27px;
+    font-size: 18px;
+    font-weight: bold;
+    color: #333333;
+  }
+  .desc {
+    line-height: 24px;
+    font-size: 13px;
+    color: #999;
   }
 `
