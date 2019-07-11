5f43d569c6d4d5660b17610e7a56f77bc0ebfce4
changxianglin <281532612@qq.com>
2019-07-12 00:27:22 +0800

    write some layout

--- imooc/jianshu/src/page/Detail/index.js
+++ imooc/jianshu/src/page/Detail/index.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import { DetailWrapper, Header, Content } from './style'
 
 export default class Detail extends Component {
   render() {
     return (
-      <div>
-        DETAIL
-      </div>
+      <DetailWrapper>
+        <Header>不谈恋爱，b事没有</Header>
+        <Content>
+          <img src = '//upload-images.jianshu.io/upload_images/9059760-5c8c7cd779742351.jpeg?imageMogr2/auto-orient/strip%7CimageView2/2/w/576/format/webp' />
+          <p>但这些事你都没有为我做过，发现他朋友圈满屏都是他的<b>新女朋友</b>，他们一起旅游、兜风、看演唱会，他甚至还为她戒掉了游戏，原来他也会秀恩爱啊。</p>
+          <p>但这些事你都没有为我做过，发现他朋友圈满屏都是他的新女朋友，他们一起旅游、兜风、看演唱会，他甚至还为她戒掉了游戏，原来他也会秀恩爱啊。</p>
+          <p>但这些事你都没有为我做过，发现他朋友圈满屏都是他的新女朋友，他们一起旅游、兜风、看演唱会，他甚至还为她戒掉了游戏，原来他也会秀恩爱啊。</p>
+          <p>但这些事你都没有为我做过，发现他朋友圈满屏都是他的新女朋友，他们一起旅游、兜风、看演唱会，他甚至还为她戒掉了游戏，原来他也会秀恩爱啊。</p>
+        </Content>
+      </DetailWrapper>
     )
   }
 }
