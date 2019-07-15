e7c534c2d2151f4da7450743fbfa8b21838ef340
changxianglin <281532612@qq.com>
2019-07-16 00:17:06 +0800

    use useeffect

--- imooc/train-ticket/src/App.jsx
+++ imooc/train-ticket/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 
 class App2 extends Component {
@@ -47,4 +47,63 @@ class App2 extends Component {
   }
 }
 
-export default App2;
+function App(props) {
+  const [count, setCount] = useState(0)
+  const [size, setSize] = useState({
+    width: document.documentElement.clientWidth,
+    height: document.documentElement.clientHeight,
+  })
+
+  const onResize = () => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+    })
+  }
+
+  useEffect(() => {
+    console.log('count:',  count)
+  }, [count])
+
+  useEffect(() => {
+    document.title = count
+  })
+
+  useEffect(() => {
+    window.addEventListener('resize', onResize, false)
+
+    return () => {
+      window.removeEventListener('resize', onResize, false)
+    }
+  }, [])
+
+  const onClick = () => {
+    console.log('click')
+  }
+
+  useEffect(() => {
+    document.querySelector('#size').addEventListener('click', onClick, false)
+
+    return () => {
+      document.querySelector('#size').removeEventListener('click', onClick, false)
+    }
+  })
+
+  return (
+    <div>
+      <button
+      type = 'button'
+      onClick = {() => {setCount(count + 1)}}
+    >
+      Click({count})
+    </button>
+    {
+      count % 2 ?
+      <span id = 'size'>size: { size.width}x{size.height}</span>:
+      <p id = 'size'>size: { size.width}x{size.height}</p>
+    }
+    </div>
+  ) 
+}
+
+export default App;
