8055f09db5a8b0f85e5802abe15be432ea28eb76
changxianglin <281532612@qq.com>
2019-07-15 23:58:24 +0800

    use hooks

--- imooc/train-ticket/src/App.jsx
+++ imooc/train-ticket/src/App.jsx
@@ -1,35 +1,50 @@
-import React, { Component, PureComponent, memo } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-const Foo = memo(function Foo (props) {
-    console.log('Foo render')
-    return <div>{props.person.age}</div>
-  });
-
-class App extends Component {
+class App2 extends Component {
   state = {
     count: 0,
-    person: {
-      age: 1,
+    size: {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
     },
   }
 
+  onResize = () => {
+    this.setState({
+      size: {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight,
+      }
+    })
+  }
+
+  componentDidMount() {
+    document.title = this.state.count
+
+    window.addEventListener('resize', this.onResize, false)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.onResize, false)
+  }
+
+  componentDidUpdate() {
+    document.title = this.state.count
+  }
+
   render() {
-    const person = this.state.person
+    const { count, size } = this.state
     return (
-      <div>
-        <button 
-          onClick = {() => {
-            person.age++
-            this.setState({
-              count: this.state.count + 1
-            })
-          }}>
-          ADD</button>
-        <Foo person = {person} cb = {() => {}} />
-      </div>
-    ) 
+      <button
+        type = 'button'
+        onClick = {() => this.setState({ count: count + 1})}
+      >
+        Click({count})
+        size: { size.width}x{size.height}
+      </button>
+    )
   }
 }
 
-export default App;
+export default App2;
