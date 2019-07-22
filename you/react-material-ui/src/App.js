d1e03c1507fb9c34eb634bcce86d8d4cb352f4bc
changxianglin <281532612@qq.com>
2019-07-22 23:42:31 +0800

    create rem and to px

--- you/react-material-ui/src/App.js
+++ you/react-material-ui/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar'
+import Test from './components/Test/Test'
 import './App.css';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Test />
     </div>
   );
 }
---
+++ you/react-material-ui/src/components/Test/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+function Test() {
+  return (
+    <div className = 'test'>
+      TEST
+    </div>
+  )
+}
+
+export default Test
